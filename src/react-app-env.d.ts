403ba3b4be006e710709fcfb0c2b2183f493c66c
Ross Bulat <ross@jkrbinvestments.com>
2022-03-25 18:08:03 +0700

    Updates

--- src/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 @rossbulat/polkadot-staking-experience authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-/// <reference types="react-scripts" />
-declare module 'che-react-number-easing';
-declare module 'faler';
--- src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 @rossbulat/polkadot-staking-experience authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
