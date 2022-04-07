950a70115d652dd9d19c9bf703d5d4c7254db78c
Ross Bulat <ross@jkrbinvestments.com>
2022-04-07 11:09:36 +0700

    opactiy

--- src/library/Loaders/Announcement.tsx
+++ src/library/Loaders/Announcement.tsx
@@ -11,7 +11,7 @@ export const Announcement = () => {
       width='100%'
       backgroundColor="#e1e1e1"
       foregroundColor="#dadada"
-      opacity={0.75}
+      opacity={0.6}
     >
       <rect x="0" y="0" rx="0.5rem" ry="0.5rem" width="100%" height="100%" />
     </ContentLoader>
