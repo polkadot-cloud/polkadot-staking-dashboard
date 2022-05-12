fc42d35f09c87e12deafe126c296ec19c3162cee
Ross Bulat <ross@jkrbinvestments.com>
2022-05-12 14:23:58 +0100

    abstraction

---
+++ src/modals/ConnectAccounts/Accounts.tsx
@@ -0,0 +1,88 @@
+// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { useEffect } from 'react';
+import { Separator } from './Wrapper';
+import { useConnect } from '../../contexts/Connect';
+import { useModal } from '../../contexts/Modal';
+import Identicon from '../../library/Identicon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+export const Accounts = (props: any) => {
+
+  const { setSection } = props;
+
+  const modal = useModal();
+
+  const {
+    getAccount,
+    connectToAccount,
+    disconnectFromAccount,
+    activeWallet,
+    activeAccount,
+  }: any = useConnect();
+
+  let { accounts } = useConnect();
+
+  const activeAccountMeta = getAccount(activeAccount);
+
+  // remove active account from connect list
+  accounts = accounts.filter((item: any) => item.address !== activeAccount);
+
+  // back to wallet section if none active
+  useEffect(() => {
+    if (activeWallet === null) {
+      setSection(0);
+    }
+  }, [activeWallet]);
+
+  return (
+    <>
+      <h2>{activeAccount === '' ? 'Connect' : 'Switch'} Account</h2>
+      <div className='head'>
+        <button onClick={() => setSection(0)}>
+          <FontAwesomeIcon icon={faChevronLeft} transform="shrink-5" />
+          &nbsp;Back to Wallets
+        </button>
+      </div>
+
+      {activeAccount !== ''
+        ?
+        <button className='item' onClick={() => { disconnectFromAccount(); }}>
+          <div>
+            <Identicon value={activeAccountMeta?.address} size={26} />
+            &nbsp; {activeAccountMeta?.meta?.name}
+          </div>
+          <div className='danger'>Disconnect </div>
+        </button>
+        : <button className='item' disabled>
+          <div>No Account Connected</div>
+          <div></div>
+        </button>
+      }
+      <Separator />
+
+      {accounts.map((item: any, index: number) => {
+        const { address, meta } = item;
+        const { name } = meta;
+
+        return (
+          <button className='item' key={`switch_acnt_${index}`} onClick={() => {
+            connectToAccount(item);
+            modal.setStatus(2);
+          }}>
+            <div>
+              <Identicon value={address} size={26} />
+              &nbsp; {name}
+            </div>
+            <div>
+            </div>
+          </button>
+        );
+      })}
+    </>
+  )
+}
+
+export default Accounts;
--- src/modals/ConnectAccounts/index.tsx
+++ src/modals/ConnectAccounts/index.tsx
@@ -2,13 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useState, useEffect } from 'react';
-import { Wrapper, Separator } from './Wrapper';
+import { Wrapper } from './Wrapper';
 import { useConnect } from '../../contexts/Connect';
 import { useModal } from '../../contexts/Modal';
-import Identicon from '../../library/Identicon';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { Wallets } from './Wallets';
+import { Accounts } from './Accounts';
 
 export const ConnectAccounts = () => {
 
@@ -18,16 +16,11 @@ export const ConnectAccounts = () => {
   const _section = config?.section ?? null;
 
   const {
-    getAccount,
-    connectToAccount,
-    disconnectFromAccount,
     activeWallet,
     activeAccount,
   }: any = useConnect();
 
   let { accounts } = useConnect();
-
-  const activeAccountMeta = getAccount(activeAccount);
 
   // active section of modal
   const [section, setSection] = useState(
@@ -47,54 +40,7 @@ export const ConnectAccounts = () => {
   return (
     <Wrapper>
       {section === 0 && <Wallets setSection={setSection} />}
-
-      {section === 1 &&
-        <>
-          <h2>{activeAccount === '' ? 'Connect' : 'Switch'} Account</h2>
-          <div className='head'>
-            <button onClick={() => setSection(0)}>
-              <FontAwesomeIcon icon={faChevronLeft} transform="shrink-5" />
-              &nbsp;Back to Wallets
-            </button>
-          </div>
-
-          {activeAccount !== ''
-            ?
-            <button className='item' onClick={() => { disconnectFromAccount(); }}>
-              <div>
-                <Identicon value={activeAccountMeta?.address} size={26} />
-                &nbsp; {activeAccountMeta?.meta?.name}
-              </div>
-              <div className='danger'>Disconnect </div>
-            </button>
-            : <button className='item' disabled>
-              <div>No Account Connected</div>
-              <div></div>
-            </button>
-          }
-          <Separator />
-
-          {accounts.map((item: any, index: number) => {
-
-            const { address, meta } = item;
-            const { name } = meta;
-
-            return (
-              <button className='item' key={`switch_acnt_${index}`} onClick={() => {
-                connectToAccount(item);
-                modal.setStatus(2);
-              }}>
-                <div>
-                  <Identicon value={address} size={26} />
-                  &nbsp; {name}
-                </div>
-                <div>
-                </div>
-              </button>
-            );
-          })}
-        </>
-      }
+      {section === 1 && <Accounts setSection={setSection} />}
     </Wrapper>
   )
 }
