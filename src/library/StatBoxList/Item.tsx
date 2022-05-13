e96dc7cca5de219c574598a9a53b1f6b034ef408
Ross Bulat <ross@jkrbinvestments.com>
2022-05-13 11:08:19 +0100

    abstract formats

--- src/library/StatBoxList/Item.tsx
+++ src/library/StatBoxList/Item.tsx
@@ -2,135 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { StatBoxWrapper } from './Wrapper';
-import NumberEasing from 'che-react-number-easing';
-import { StatPie } from '../../library/Graphs/StatBoxPie';
-import { OpenAssistantIcon } from '../../library/OpenAssistantIcon';
+import { Pie } from './Pie';
+import { Number } from './Number';
+import { Text } from './Text';
 
-const PieStatBox = (props: any) => {
-  const { label, value, value2, total, unit, tooltip, assistant } = props;
-  let assist = assistant !== undefined;
-  let page = assistant?.page ?? '';
-  let key = assistant?.key ?? '';
-
-  let showValue = !(value === 0 && total !== 0);
-  let showTotal = !(total === undefined || !total);
-  return (
-    <StatBox>
-      <div className="content chart">
-        <div className="chart">
-          <StatPie value={value} value2={value2} />
-          {tooltip && (
-            <div className="tooltip">
-              <p>{tooltip}</p>
-            </div>
-          )}
-        </div>
-
-        <div className="labels">
-          <>
-            <h2>
-              {showValue ? (
-                <>
-                  <NumberEasing
-                    ease="quintInOut"
-                    precision={2}
-                    speed={250}
-                    trail={false}
-                    value={value}
-                    useLocaleString={true}
-                  />
-                  {unit && <>&nbsp;{unit}</>}
-
-                  {showTotal && (
-                    <span className="total">
-                      /{' '}
-                      <NumberEasing
-                        ease="quintInOut"
-                        precision={2}
-                        speed={250}
-                        trail={false}
-                        value={total}
-                        useLocaleString={true}
-                      />
-                      {unit && <>&nbsp;{unit}</>}
-                    </span>
-                  )}
-                </>
-              ) : (
-                <>0</>
-              )}
-            </h2>
-            <h4>
-              {label}
-              {assist && <OpenAssistantIcon page={page} title={key} />}
-            </h4>
-          </>
-        </div>
-      </div>
-    </StatBox>
-  );
-};
-
-const TextStatBox = (props: any) => {
-  const { label, value, assistant } = props;
-
-  let assist = assistant !== undefined;
-  let page = assistant?.page ?? '';
-  let key = assistant?.key ?? '';
-  return (
-    <StatBox>
-      <div className="content chart">
-        <div className="labels">
-          <>
-            <h1>{value}</h1>
-            <h4>
-              {label}
-              {assist && <OpenAssistantIcon page={page} title={key} />}
-            </h4>
-          </>
-        </div>
-      </div>
-    </StatBox>
-  );
-};
-
-const NumberStatBox = (props: any) => {
-  const { label, value, unit, assistant } = props;
-
-  let assist = assistant !== undefined;
-  let page = assistant?.page ?? '';
-  let key = assistant?.key ?? '';
-
-  let currency = props.currency ?? '';
-  return (
-    <StatBox>
-      <div className="content chart">
-        <div className="labels">
-          <>
-            <h2>
-              <NumberEasing
-                ease="quintInOut"
-                precision={2}
-                speed={250}
-                trail={false}
-                value={value}
-                useLocaleString={true}
-                currency={currency}
-              />
-              {unit && <>&nbsp;{unit}</>}
-            </h2>
-            <h4>
-              {label}
-              {assist && <OpenAssistantIcon page={page} title={key} />}
-            </h4>
-          </>
-        </div>
-      </div>
-    </StatBox>
-  );
-};
-
-const StatBox = ({ children }: any) => {
+export const StatBox = ({ children }: any) => {
   return (
     <StatBoxWrapper
       whileHover={{ scale: 1.02 }}
@@ -148,11 +24,14 @@ const StatBox = ({ children }: any) => {
 const StatBoxListItem = ({ format, params }: any) => {
   switch (format) {
     case 'chart-pie':
-      return <PieStatBox {...params} />;
+      return <Pie {...params} />;
+
     case 'number':
-      return <NumberStatBox {...params} />;
+      return <Number {...params} />;
+
     case 'text':
-      return <TextStatBox {...params} />;
+      return <Text {...params} />;
+
     default:
       return null;
   }
---
+++ src/library/StatBoxList/Number.tsx
@@ -0,0 +1,41 @@
+// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { StatBox } from './Item';
+import NumberEasing from 'che-react-number-easing';
+import { OpenAssistantIcon } from '../../library/OpenAssistantIcon';
+
+export const Number = (props: any) => {
+
+  const { label, value, unit, assistant } = props;
+
+  let assist = assistant !== undefined;
+  let page = assistant?.page ?? '';
+  let key = assistant?.key ?? '';
+
+  let currency = props.currency ?? '';
+  return (
+    <StatBox>
+      <div className="content chart">
+        <div className="labels">
+          <h2>
+            <NumberEasing
+              ease="quintInOut"
+              precision={2}
+              speed={250}
+              trail={false}
+              value={value}
+              useLocaleString={true}
+              currency={currency}
+            />
+            {unit && <>&nbsp;{unit}</>}
+          </h2>
+          <h4>
+            {label}
+            {assist && <OpenAssistantIcon page={page} title={key} />}
+          </h4>
+        </div>
+      </div>
+    </StatBox>
+  );
+};
