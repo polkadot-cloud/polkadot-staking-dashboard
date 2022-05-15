af852bcefd36b309c1e292a1ccfe4bcb7077d067
Ross Bulat <ross@jkrbinvestments.com>
2022-05-15 20:38:23 +0100

    extra loader

--- src/library/Loaders/Announcement.tsx
+++ src/library/Loaders/Announcement.tsx
@@ -10,15 +10,27 @@ export const Announcement = () => {
   const { mode } = useTheme();
 
   return (
-    <ContentLoader
-      height={90}
-      width='100%'
-      backgroundColor={defaultThemes.loader.background[mode]}
-      foregroundColor={defaultThemes.loader.foreground[mode]}
-      opacity={0.2}
-    >
-      <rect x="0" y="0" rx="0.5rem" ry="0.5rem" width="100%" height="100%" />
-    </ContentLoader>
+    <>
+      <ContentLoader
+        height={90}
+        width='100%'
+        backgroundColor={defaultThemes.loader.background[mode]}
+        foregroundColor={defaultThemes.loader.foreground[mode]}
+        opacity={0.2}
+        style={{ marginTop: '0.75rem', marginBottom: '0.75rem' }}
+      >
+        <rect x="0" y="0" rx="0.5rem" ry="0.5rem" width="100%" height="100%" />
+      </ContentLoader>
+      <ContentLoader
+        height={90}
+        width='100%'
+        backgroundColor={defaultThemes.loader.background[mode]}
+        foregroundColor={defaultThemes.loader.foreground[mode]}
+        opacity={0.2}
+      >
+        <rect x="0" y="0" rx="0.5rem" ry="0.5rem" width="100%" height="100%" />
+      </ContentLoader>
+    </>
   )
 }
 
