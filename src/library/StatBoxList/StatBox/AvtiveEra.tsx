8b7b10aea558ecbd4254b3d411e536a40690e96d
Ross Bulat <ross@jkrbinvestments.com>
2022-05-18 10:47:18 +0100

    eraTimeLeft

--- src/library/StatBoxList/StatBox/AvtiveEra.tsx
+++ src/library/StatBoxList/StatBox/AvtiveEra.tsx
@@ -1,13 +1,21 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import moment from 'moment';
 import { useNetworkMetrics } from '../../../contexts/Network';
 import { useSessionEra } from '../../../contexts/SessionEra';
+import { useEraTimeLeft } from '../../../library/Hooks/useEraTimeLeft';
 import { Pie } from '../Pie';
 
 const ActiveEraStatBox = () => {
   const { metrics } = useNetworkMetrics();
   const { sessionEra } = useSessionEra();
+  const eraTimeLeft = useEraTimeLeft();
+
+  // format era time left
+  let _timeleft = moment.duration(eraTimeLeft * 1000, 'milliseconds');
+  let timeleft = _timeleft.hours() + ":" + _timeleft.minutes() + ":" + _timeleft.seconds();
+
   const params = {
     label: 'Active Era',
     stat: {
@@ -18,6 +26,7 @@ const ActiveEraStatBox = () => {
       value1: sessionEra.eraProgress,
       value2: sessionEra.eraLength - sessionEra.eraProgress,
     },
+    tooltip: timeleft,
     assistant: {
       page: 'validators',
       key: 'Era',
