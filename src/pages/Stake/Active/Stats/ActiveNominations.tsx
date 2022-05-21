abb0e2bab053fbec073f5d0faa49c4a5368f7d44
Ross Bulat <ross@jkrbinvestments.com>
2022-05-21 19:33:27 +0100

    fix infinite re-render

--- src/pages/Stake/Active/Stats/ActiveNominations.tsx
+++ src/pages/Stake/Active/Stats/ActiveNominations.tsx
@@ -1,56 +1,29 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState, useEffect, useMemo } from 'react';
 import { useStaking } from '../../../../contexts/Staking';
-import { useBalances } from '../../../../contexts/Balances';
-import { useConnect } from '../../../../contexts/Connect';
 import { Pie } from '../../../../library/StatBoxList/Pie';
 
 export const ActiveNominationsStatBox = () => {
-  const { activeAccount } = useConnect();
   const { getNominationsStatus } = useStaking();
-  const { getAccountNominations }: any = useBalances();
-  const nominations = getAccountNominations(activeAccount);
+  const statuses = getNominationsStatus();
 
-  // handle nomination statuses
-  const [nominationsStatus, setNominationsStatus]: any = useState({
-    total: 0,
-    inactive: 0,
-    active: 0,
-  });
-
-  const nominationStatuses = useMemo(
-    () => getNominationsStatus(),
-    [nominations]
-  );
-
-  useEffect(() => {
-    const statuses = nominationStatuses;
-    const total = Object.values(statuses).length;
-    const _active: any = Object.values(statuses).filter(
-      (_v: any) => _v === 'active'
-    ).length;
-
-    setNominationsStatus({
-      total,
-      inactive: total - _active,
-      active: _active,
-    });
-  }, [nominationStatuses]);
+  const total = Object.values(statuses).length;
+  const active =
+    Object.values(statuses).filter((_v: any) => _v === 'active').length ?? 0;
 
   const params = {
     label: 'Active Nominations',
     stat: {
-      value: nominationsStatus.active,
-      total: nominationsStatus.total,
+      value: active,
+      total,
       unit: '',
     },
     graph: {
-      value1: nominationsStatus.active,
-      value2: nominationsStatus.active ? 0 : 1,
+      value1: active,
+      value2: active ? 0 : 1,
     },
-    tooltip: `${nominationsStatus.active ? 'Active' : 'Inactive'}`,
+    tooltip: `${active ? 'Active' : 'Inactive'}`,
     assistant: {
       page: 'stake',
       key: 'Nominations',
