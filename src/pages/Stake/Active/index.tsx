d7ac6113d35c9cbc53901b0ff38f1d5f41a1b555
Ross Bulat <ross@jkrbinvestments.com>
2022-05-22 17:54:53 +0100

    rm import

--- src/pages/Stake/Active/index.tsx
+++ src/pages/Stake/Active/index.tsx
@@ -1,7 +1,6 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState, useEffect, useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faRedoAlt,
