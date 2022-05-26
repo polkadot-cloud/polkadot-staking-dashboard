854fdc95c7b3ba04ff56c8f60fefa54302abc7d6
hamidra <hamid.alipour@gmail.com>
2022-05-26 10:45:17 -0700

    Show nominations view when is not pool nominatior

--- src/pages/Pools/ManagePool/index.tsx
+++ src/pages/Pools/ManagePool/index.tsx
@@ -1,7 +1,6 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState } from 'react';
 import { usePools } from 'contexts/Pools';
 import { useModal } from 'contexts/Modal';
 import { PageRowWrapper } from '../../../Wrappers';
@@ -12,6 +11,7 @@ import {
 import { OpenAssistantIcon } from '../../../library/OpenAssistantIcon';
 import { Roles } from './Roles';
 import { GenerateNominations } from '../../Stake/GenerateNominations';
+import { Nominations } from '../../Stake/Active/Nominations';
 import { Button } from '../../../library/Button';
 import { ManageWrapper } from './Wrappers';
 
@@ -26,32 +26,38 @@ export const ManagePool = () => {
             <Roles />
           </div>
           <div>
-            <SectionHeaderWrapper withAction>
-              <h3>
-                Generate Nominations
-                <OpenAssistantIcon page="stake" title="Nominations" />
-              </h3>
-              <div>
-                <Button
-                  small
-                  inline
-                  primary
-                  title="Nominate"
-                  disabled={!isNominator()}
-                  onClick={() => openModalWith('NominatePool', {}, 'small')}
+            {isNominator() ? (
+              <>
+                <SectionHeaderWrapper withAction>
+                  <h3>
+                    Generate Nominations
+                    <OpenAssistantIcon page="stake" title="Nominations" />
+                  </h3>
+                  <div>
+                    <Button
+                      small
+                      inline
+                      primary
+                      title="Nominate"
+                      disabled={!isNominator()}
+                      onClick={() => openModalWith('NominatePool', {}, 'small')}
+                    />
+                  </div>
+                </SectionHeaderWrapper>
+                <GenerateNominations
+                  batchKey="generate_pool_nominations"
+                  nominations={targets.nominations}
+                  setters={[
+                    {
+                      set: setTargets,
+                      current: targets,
+                    },
+                  ]}
                 />
-              </div>
-            </SectionHeaderWrapper>
-            <GenerateNominations
-              batchKey="generate_pool_nominations"
-              nominations={targets.nominations}
-              setters={[
-                {
-                  set: setTargets,
-                  current: targets,
-                },
-              ]}
-            />
+              </>
+            ) : (
+              <Nominations />
+            )}
           </div>
         </ManageWrapper>
       </SectionWrapper>
