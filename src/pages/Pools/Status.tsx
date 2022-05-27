97d840cd989080f8d54754387120e56d5d259c59
Ross Bulat <ross@jkrbinvestments.com>
2022-05-27 18:07:14 +0100

    height fix

--- src/pages/Pools/Status.tsx
+++ src/pages/Pools/Status.tsx
@@ -70,7 +70,7 @@ export const Status = () => {
     : undefined;
 
   return (
-    <SectionWrapper style={{ height: 300 }}>
+    <SectionWrapper height="300">
       <Stat
         label="Status"
         assistant={['pools', 'Pool Status']}
