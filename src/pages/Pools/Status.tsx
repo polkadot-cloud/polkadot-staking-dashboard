7a00c33c5ba15c054f3b0815645cbe7a921931f8
Ross Bulat <ross@jkrbinvestments.com>
2022-05-27 23:09:29 +0100

    show pool nomination status

--- src/pages/Pools/Status.tsx
+++ src/pages/Pools/Status.tsx
@@ -1,6 +1,8 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { useStaking } from 'contexts/Staking';
+import { useUi } from 'contexts/UI';
 import { Separator } from '../../Wrappers';
 import { SectionWrapper } from '../../library/Graphs/Wrappers';
 import { useApi } from '../../contexts/Api';
@@ -11,9 +13,20 @@ import { APIContextInterface } from '../../types/api';
 
 export const Status = () => {
   const { network } = useApi() as APIContextInterface;
-  const { unit } = network;
-  const { membership, isOwner } = usePools();
+  const { isSyncing } = useUi();
+  const { membership, isOwner, getNominationsStatus, targets } = usePools();
   const { openModalWith } = useModal();
+  const { inSetup } = useStaking();
+  const { unit } = network;
+
+  // get nomination status
+  const nominationStatuses = getNominationsStatus();
+  const statuses: any =
+    nominationStatuses === undefined ? [] : nominationStatuses;
+
+  const active: any = Object.values(statuses).filter(
+    (_v: any) => _v === 'active'
+  ).length;
 
   // Pool status `Stat` props
   const labelMembership = membership
@@ -78,6 +91,24 @@ export const Status = () => {
         stat={labelRewards}
         buttons={buttonsRewards}
       />
+      {membership && (
+        <>
+          <Separator />
+          <Stat
+            label="Pool Status"
+            assistant={['stake', 'Staking Status']}
+            stat={
+              inSetup() || isSyncing
+                ? 'Inactive: Not Nominating'
+                : !targets.nominations.length
+                ? 'Inactive: Not Nominating'
+                : active
+                ? 'Actively Nominating with Pool Funds'
+                : 'Waiting for Active Nominations'
+            }
+          />
+        </>
+      )}
     </SectionWrapper>
   );
 };
