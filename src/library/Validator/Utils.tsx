499d784adf4f86c37199a95782170be7ab34b2c4
Ross Bulat <ross@jkrbinvestments.com>
2022-05-28 23:05:47 +0100

    support byte-encoded identities

--- src/library/Validator/Utils.tsx
+++ src/library/Validator/Utils.tsx
@@ -1,6 +1,8 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { u8aToString, u8aUnwrapBytes } from '@polkadot/util';
+
 export const getIdentityDisplay = (identity: any, _superIdentity: any) => {
   // first check identity of validator
 
@@ -9,17 +11,33 @@ export const getIdentityDisplay = (identity: any, _superIdentity: any) => {
   // legal.Raw
   display = display === null ? identity?.info?.legal.Raw ?? null : display;
 
+  // check if identity has been byte encoded
+  const displayAsBytes = u8aToString(u8aUnwrapBytes(display));
+
+  if (displayAsBytes !== '') {
+    return displayAsBytes;
+  }
   if (display !== null) {
     return display;
   }
 
   // if still null, check super identity
-
   const superIdentity = _superIdentity?.identity ?? null;
 
+  // display.Raw
   display = superIdentity?.info?.display?.Raw ?? null;
   // legal.Raw
   display = display === null ? superIdentity?.info?.legal.Raw ?? null : display;
+
+  // check if super identity has been byte encoded
+  const superIdentityAsBytes = u8aToString(u8aUnwrapBytes(display));
+
+  if (superIdentityAsBytes !== '') {
+    return superIdentityAsBytes;
+  }
+  if (display !== null) {
+    return display;
+  }
 
   return display;
 };
