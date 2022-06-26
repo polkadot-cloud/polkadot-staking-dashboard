066f66259d1459eb73cd006932be3e44eaf03345
Ross Bulat <ross@jkrbinvestments.com>
2022-06-26 12:29:22 +0100

    fetch more payout pages

--- src/contexts/Subscan.tsx
+++ src/contexts/Subscan.tsx
@@ -1,6 +1,7 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import moment from 'moment';
 import React, { useState, useEffect } from 'react';
 import { API_ENDPOINTS, API_SUBSCAN_KEY } from 'consts';
 import { APIContextInterface } from 'types/api';
@@ -56,30 +57,58 @@ export const SubscanProvider = ({
       return;
     }
 
-    let res: any = await fetch(
-      network.subscanEndpoint + API_ENDPOINTS.subscanRewardSlash,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'X-API-Key': API_SUBSCAN_KEY,
-        },
-        body: JSON.stringify({
-          row: 100,
-          page: 0,
-          address: activeAccount,
-        }),
-        method: 'POST',
-      }
-    );
-
-    res = await res.json();
-    if (res.message === 'Success') {
-      if (getServices().includes('subscan')) {
-        if (res.data.list !== null) {
-          setPayouts(res.data.list);
-        } else {
-          setPayouts([]);
+    const fetchPayoutPage = async (page: number) => {
+      const res: Response = await fetch(
+        network.subscanEndpoint + API_ENDPOINTS.subscanRewardSlash,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': API_SUBSCAN_KEY,
+          },
+          body: JSON.stringify({
+            row: 100,
+            page,
+            address: activeAccount,
+          }),
+          method: 'POST',
         }
+      );
+      const resJson: any = await res.json();
+      return resJson;
+    };
+
+    // fetch 2 pages of results if subscan is enabled
+    if (getServices().includes('subscan')) {
+      let _payouts: Array<any> = [];
+
+      // fetch 3 pages of results
+      const results = await Promise.all([
+        fetchPayoutPage(0),
+        fetchPayoutPage(1),
+        fetchPayoutPage(2),
+      ]);
+
+      // user may have turned off service while results were fetching.
+      // test again whether subscan service is still active.
+      if (getServices().includes('subscan')) {
+        const maxDays = 60;
+
+        for (const result of results) {
+          if (!result?.data?.list) {
+            break;
+          }
+          const { list } = result.data;
+          const last = list[list.length - 1];
+          const timestamp = last.block_timestamp;
+
+          // if we have reached max amount of days needed, break loop early
+          const daysSince = moment().diff(moment.unix(timestamp), 'days');
+          if (daysSince > maxDays) {
+            break;
+          }
+          _payouts = _payouts.concat(result.data.list);
+        }
+        setPayouts(_payouts);
       }
     }
   };
