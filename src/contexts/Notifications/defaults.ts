c39fcce072adce934eb838b7f15c4b82638cfe36
Ross Bulat <ross@jkrbinvestments.com>
2022-07-03 13:36:39 +0200

    add defaultNotificationsContext

---
+++ src/contexts/Notifications/defaults.ts
@@ -0,0 +1,10 @@
+// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+export const defaultNotificationsContext = {
+  // eslint-disable-next-line
+  addNotification: (n: any) => {},
+  // eslint-disable-next-line
+    removeNotification: (n: any) => {},
+  notifications: [],
+};
