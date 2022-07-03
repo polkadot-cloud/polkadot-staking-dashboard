5baac631bc9b79b6624e46fb07f57f40c9e78cc1
Ross Bulat <ross@jkrbinvestments.com>
2022-07-03 15:08:06 +0200

    menu types

--- src/contexts/Menu/defaults.ts
+++ src/contexts/Menu/defaults.ts
@@ -1,18 +1,17 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { RefObject } from 'react';
 import { MenuContextInterface } from './types';
 
 export const defaultMenuContext: MenuContextInterface = {
   openMenu: () => {},
   closeMenu: () => {},
   // eslint-disable-next-line
-  setMenuPosition: (ref: RefObject<HTMLDivElement>) => {},
+  setMenuPosition: (ref) => {},
   // eslint-disable-next-line
-  checkMenuPosition: (ref: RefObject<HTMLDivElement>) => {},
+  checkMenuPosition: (ref) => {},
   // eslint-disable-next-line
-  setMenuItems: (items: React.ReactNode[]) => {},
+  setMenuItems: (items) => {},
   open: 0,
   show: 0,
   position: [0, 0],
--- src/contexts/Menu/types.ts
+++ src/contexts/Menu/types.ts
@@ -1,14 +1,16 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import React, { RefObject } from 'react';
+
 export interface MenuContextInterface {
-  openMenu: () => any;
-  closeMenu: () => any;
-  setMenuPosition: (ref: any) => void;
-  checkMenuPosition: (ref: any) => void;
-  setMenuItems: (items: any) => void;
+  openMenu: () => void;
+  closeMenu: () => void;
+  setMenuPosition: (ref: RefObject<HTMLDivElement>) => void;
+  checkMenuPosition: (ref: RefObject<HTMLDivElement>) => void;
+  setMenuItems: (items: React.ReactNode[]) => void;
   open: number;
   show: number;
   position: [number, number];
-  items: any;
+  items: React.ReactNode[];
 }
