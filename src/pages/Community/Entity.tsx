a7e7d5f27d457246eea04a1c90994bc927e12553
Ross Bulat <ross@jkrbinvestments.com>
2022-07-15 22:50:11 +0100

    validator shuffle

--- src/pages/Community/Entity.tsx
+++ src/pages/Community/Entity.tsx
@@ -1,6 +1,7 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { useState, useEffect } from 'react';
 import { PageRowWrapper, GoBackWrapper } from 'Wrappers';
 import Button from 'library/Button';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
@@ -8,21 +9,34 @@ import { useValidators } from 'contexts/Validators';
 import ValidatorList from 'library/ValidatorList';
 import { CardWrapper } from 'library/Graphs/Wrappers';
 import { useApi } from 'contexts/Api';
+import { shuffle } from 'Utils';
 import { ItemsWrapper } from './Wrappers';
 import { Item } from './Item';
 import { useCommunitySections } from './context';
 
 export const Entity = () => {
   const { isReady } = useApi();
-  const { validators: allValidators } = useValidators();
+  const { validators: allValidators, removeValidatorMetaBatch } =
+    useValidators();
   const { setActiveSection, activeItem } = useCommunitySections();
   const { name, validators } = activeItem;
 
-  // TODO: move filters to ValidatorList context
-  // TODO: wrap ValidatorList context around headless components that need it.
   const entityValidators = allValidators.filter((v: any) =>
     validators.includes(v.address)
   );
+
+  const [shuffledValidators, setShuffledValidators] = useState(
+    shuffle(entityValidators)
+  );
+
+  useEffect(() => {
+    removeValidatorMetaBatch(batchKey);
+    const newShuffledValidators = shuffle([...shuffledValidators]);
+    setShuffledValidators(shuffle(newShuffledValidators));
+  }, [name]);
+
+  // TODO: move filters to ValidatorList context
+  // TODO: wrap ValidatorList context around headless components that need it.
 
   const container = {
     hidden: { opacity: 0 },
@@ -58,14 +72,14 @@ export const Entity = () => {
           </div>
         ) : (
           <>
-            {validators.length === 0 && (
+            {shuffledValidators.length === 0 && (
               <div className="item">
                 <h3>Fetching validators...</h3>
               </div>
             )}
-            {validators.length > 0 && (
+            {shuffledValidators.length > 0 && (
               <ValidatorList
-                validators={entityValidators}
+                validators={shuffledValidators}
                 batchKey={batchKey}
                 title={`${name}'s Validators`}
                 selectable={false}
