94352c4b481d8003f29a5718efdbee10ef171bdd
tutu <tutu@amforc.com>
2022-08-02 22:12:04 +0200

    add amforc validators

--- src/config/validators/index.ts
+++ src/config/validators/index.ts
@@ -6,6 +6,7 @@
  * import { ReactComponent as ValidatorEntityName } from './thumbnails/validatorEntityName.svg';
  */
 import { ReactComponent as Placeholder } from './thumbnails/placeholder.svg';
+import { ReactComponent as Amforc } from './thumbnails/amforc.svg';
 
 export const VALIDATOR_COMMUNITY = [
   {
@@ -30,6 +31,25 @@ export const VALIDATOR_COMMUNITY = [
         'GeejAnVcS68oHMdYzXGSkfahPQRi1Q5jYKuZt7Abi6nQbaK',
       ],
       westend: ['5GYaYNVq6e855t5hVCyk4Wuqssaf6ADTrvdPZ3QXyHvFXTip'],
+    },
+  },
+  {
+    name: 'Amforc',
+    Thumbnail: Amforc,
+    bio: 'We are a independent and experienced staking provider from Switzerland. We run our validators in a hybrid cloud setup across multiple geographical locations. Our validators run significantly above average and close to no blocks are missed as para-validator.',
+    email: 'staking@amforc.com',
+    twitter: '@amforcag',
+    website: 'https://amforc.com/',
+    validators: {
+      polkadot: [
+        '1y6CPLgccsysCEii3M7jQF834GZsz9A3HMcZz3w7RjGPpBL',
+      ],
+      kusama: [
+        'DVUNoinHdSNfismcrFaBwdJfysxc7A48QkNvTDnTSPXPw3q',
+        'DpLatoXXBiSAPooF17bzUZGo7huNB7USfRqd2SgL6RBy2zr',
+        'E8zY6KdAH1vuKKMaPdHYLAziht32v2BLqXk6qBw7WtiV2Dv',
+        'Ff3xdNrXA47svhiTJHj9uNhxLo29PYjYcJ9cUseAd9FK6iQ',
+      ],
     },
   },
 ];
