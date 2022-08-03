8b5350fee16c8fb739b8c82456742dca2310e1a4
Ross Bulat <ross@jkrbinvestments.com>
2022-08-03 10:54:56 +0100

    add polkachu

--- src/config/validators/index.ts
+++ src/config/validators/index.ts
@@ -7,6 +7,7 @@
  */
 import { ReactComponent as Amforc } from './thumbnails/amforc.svg';
 import { ReactComponent as HighStake } from './thumbnails/highstake.svg';
+import { ReactComponent as Polkachu } from './thumbnails/polkachu.svg';
 import { ReactComponent as Wojdot } from './thumbnails/wojdot.svg';
 
 export const VALIDATOR_COMMUNITY = [
@@ -41,6 +42,24 @@ export const VALIDATOR_COMMUNITY = [
         'DpLatoXXBiSAPooF17bzUZGo7huNB7USfRqd2SgL6RBy2zr',
         'E8zY6KdAH1vuKKMaPdHYLAziht32v2BLqXk6qBw7WtiV2Dv',
         'Ff3xdNrXA47svhiTJHj9uNhxLo29PYjYcJ9cUseAd9FK6iQ',
+      ],
+    },
+  },
+  {
+    name: 'polkachu.com',
+    Thumbnail: Polkachu,
+    bio: 'Polkachu Validators helps investors compound their crypto investments with low commission and advanced support.',
+    email: 'hello@polkachu.com',
+    twitter: '@polka_chu',
+    website: 'https://polkachu.com/',
+    validators: {
+      polkadot: ['15ym3MDSG4WPABNoEtx2rAzBB1EYWJDWbWYpNg1BwuWRAQcY'],
+      kusama: [
+        'CsKvJ4fdesaRALc5swo5iknFDpop7YUwKPJHdmUvBsUcMGb',
+        'GpyTMuLmG3ADWRxhZpHQh5rqMgNpFoNUyxA1DJAXfvsQ2Ly',
+        'CeD8Kk3QLzp2HDRSciF6YQAc2xYAPurMsHAQUGwEJgCWAf2',
+        'GZmbAW7rRi2qkMrHYzmeG2a3fS7nTaAZpjdum8QZ7CvmM7H',
+        'G1qbViqnm6yCZwEbfB4oE38ro8VqJx21zyvW7QN8zAJC2B7',
       ],
     },
   },
