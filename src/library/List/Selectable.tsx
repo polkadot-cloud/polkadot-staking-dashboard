697b0fc2e02cfd6c4f98e8a9dd7925174e8e6a08
Ross Bulat <ross@jkrbinvestments.com>
2022-09-04 15:10:54 +0700

    unused import

--- src/library/List/Selectable.tsx
+++ src/library/List/Selectable.tsx
@@ -1,7 +1,6 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
 import { SelectableWrapper } from '.';
 import { useList } from './context';
 
