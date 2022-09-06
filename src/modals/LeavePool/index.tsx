9f567e47e4ead5836b64f5e5b007ae96f226ee77
Ross Bulat <ross@jkrbinvestments.com>
2022-09-06 22:33:28 +0700

    fix leave pool icon

--- src/modals/LeavePool/index.tsx
+++ src/modals/LeavePool/index.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { UnbondAll } from 'modals/UpdateBond/Forms/UnbondAll';
 import { HeadingWrapper, PaddingWrapper } from '../Wrappers';
 
@@ -10,7 +10,7 @@ export const LeavePool = () => {
   return (
     <PaddingWrapper>
       <HeadingWrapper noPadding>
-        <FontAwesomeIcon transform="grow-2" icon={faPlus} />
+        <FontAwesomeIcon transform="grow-2" icon={faSignOutAlt} />
         Leave Pool
       </HeadingWrapper>
       <UnbondAll />
