9250f9727f904ccc5834591e8f5337b7e51b971e
Ross Bulat <ross@jkrbinvestments.com>
2022-09-16 13:49:28 +0700

    free balance fix

--- src/pages/Overview/BalanceGraph.tsx
+++ src/pages/Overview/BalanceGraph.tsx
@@ -61,10 +61,8 @@ export const BalanceGraph = () => {
 
   // graph data
   let graphStaked = planckBnToUnit(staked, units);
-  let graphFreeToStake = planckBnToUnit(
-    freeBalance.sub(poolBondOpions.active),
-    units
-  );
+  let graphFreeToStake = planckBnToUnit(freeBalance, units);
+
   let graphInPool = planckBnToUnit(poolBondOpions.active, units);
   let graphUnlocking = planckBnToUnit(unlocking, units);
 
