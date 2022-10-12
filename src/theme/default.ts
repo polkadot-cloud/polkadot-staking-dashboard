d5eba8515d5ed0a53b5d271ecc54b7ced3fae12f
Ross Bulat <ross@jkrbinvestments.com>
2022-10-12 11:03:54 +0100

    dark: lighter shadow

--- src/theme/default.ts
+++ src/theme/default.ts
@@ -91,7 +91,7 @@ export const defaultThemes: { [key: string]: any } = {
     foreground: v('#e1e1e1', '#151515'),
     background: v('#dadada', '#101010'),
   },
-  shadow: v('#dedede', '#181818'),
+  shadow: v('#dedede', '#1f1f1f'),
   status: {
     danger: {
       solid: v('red', 'red'),
