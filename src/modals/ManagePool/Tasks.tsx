7aa833a9636970e4244ec43bb5fe7d29ef55caff
Ross Bulat <ross@jkrbinvestments.com>
2022-10-13 21:54:30 +0100

    fix jsx

--- src/modals/ManagePool/Tasks.tsx
+++ src/modals/ManagePool/Tasks.tsx
@@ -43,71 +43,64 @@ export const Tasks = forwardRef((props: any, ref: any) => {
             </div>
           </button>
         )}
-        {isOwner() ||
-          (isStateToggler() && (
-            <>
-              {poolLocked ? (
-                <button
-                  type="button"
-                  className="action-button"
-                  disabled={poolDestroying}
-                  onClick={() => {
-                    setSection(1);
-                    setTask('unlock_pool');
-                  }}
-                >
-                  <div>
-                    <h3>Unlock Pool</h3>
-                    <p>Allow new members to join the pool.</p>
-                  </div>
-                  <div>
-                    <FontAwesomeIcon
-                      transform="shrink-2"
-                      icon={faChevronRight}
-                    />
-                  </div>
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="action-button"
-                  disabled={poolDestroying}
-                  onClick={() => {
-                    setSection(1);
-                    setTask('lock_pool');
-                  }}
-                >
-                  <div>
-                    <h3>Lock Pool</h3>
-                    <p>Stop new members from joining the pool.</p>
-                  </div>
-                  <div>
-                    <FontAwesomeIcon
-                      transform="shrink-2"
-                      icon={faChevronRight}
-                    />
-                  </div>
-                </button>
-              )}
+        {(isOwner() || isStateToggler()) && (
+          <>
+            {poolLocked ? (
               <button
                 type="button"
                 className="action-button"
                 disabled={poolDestroying}
                 onClick={() => {
                   setSection(1);
-                  setTask('destroy_pool');
+                  setTask('unlock_pool');
                 }}
               >
                 <div>
-                  <h3>Destroy Pool</h3>
-                  <p>Change pool to destroying state.</p>
+                  <h3>Unlock Pool</h3>
+                  <p>Allow new members to join the pool.</p>
                 </div>
                 <div>
                   <FontAwesomeIcon transform="shrink-2" icon={faChevronRight} />
                 </div>
               </button>
-            </>
-          ))}
+            ) : (
+              <button
+                type="button"
+                className="action-button"
+                disabled={poolDestroying}
+                onClick={() => {
+                  setSection(1);
+                  setTask('lock_pool');
+                }}
+              >
+                <div>
+                  <h3>Lock Pool</h3>
+                  <p>Stop new members from joining the pool.</p>
+                </div>
+                <div>
+                  <FontAwesomeIcon transform="shrink-2" icon={faChevronRight} />
+                </div>
+              </button>
+            )}
+            <button
+              type="button"
+              className="action-button"
+              disabled={poolDestroying}
+              onClick={() => {
+                setSection(1);
+                setTask('destroy_pool');
+              }}
+            >
+              <div>
+                <h3>Destroy Pool</h3>
+                <p>Change pool to destroying state.</p>
+              </div>
+              <div>
+                <FontAwesomeIcon transform="shrink-2" icon={faChevronRight} />
+              </div>
+            </button>
+          </>
+        )}
       </div>
     </ContentWrapper>
   );
