9cc2ed754e55d73aca5375bc74f8b02e121699c0
Ross Bulat <ross@jkrbinvestments.com>
2022-11-12 11:27:34 +0000

    tidy up locale config

--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -7,6 +7,14 @@ import { initReactI18next } from 'react-i18next';
 import pagesCn from './cn/pages.json';
 import pagesEn from './en/pages.json';
 
+// get locale from localStorage.
+const localLng = localStorage.getItem('lng');
+const lng = localLng ?? DefaultLocale;
+
+if (!localLng) {
+  localStorage.setItem('lng', DefaultLocale);
+}
+
 // construct resources.
 export const resources = {
   en: {
@@ -16,30 +24,16 @@ export const resources = {
     ...pagesCn,
   },
 };
-
-// get locale from localStorage.
-const localLocale = localStorage.getItem('locale');
-const locale = localLocale ?? DefaultLocale;
-
-if (!localLocale) {
-  localStorage.setItem('locale', DefaultLocale);
-}
-
 // configure i18n object.
-i18next.use(initReactI18next).init(
-  {
-    initImmediate: false, // setting initImediate to false, will load the resources synchronously
-    interpolation: { escapeValue: false },
-    lng: locale,
-    fallbackLng: DefaultLocale,
-    fallbackNS: FallbackNS,
-    debug: process.env.REACT_APP_DEBUG_I18N === '1',
-    resources,
-  },
-  () => {
-    /* locale error */
-  }
-);
+i18next.use(initReactI18next).init({
+  debug: process.env.REACT_APP_DEBUG_I18N === '1',
+  fallbackLng: DefaultLocale,
+  fallbackNS: FallbackNS,
+  initImmediate: true,
+  interpolation: { escapeValue: false },
+  lng,
+  resources,
+});
 
 // export i18next for context.
 export { i18next };
