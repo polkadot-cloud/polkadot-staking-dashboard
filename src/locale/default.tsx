044c191e029f8029e830f8185c1d5ed144909687
Ross Bulat <ross@jkrbinvestments.com>
2022-11-17 13:34:24 +0100

    fallback en, dynamic load support

--- src/locale/default.tsx
+++ src/locale/default.tsx
@@ -1,17 +1,12 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { DefaultLocale } from 'consts';
 import { AnyJson } from 'types';
 
-// Load a default lanauge.
+// Load language resources dynamically.
 //
-// Bootstraps i18next with a default language.
-export const loadDefault = async () => {
-  // get locale from localStorage.
-  const localLng = localStorage.getItem('lng');
-  const lng = localLng ?? DefaultLocale;
-
+// Bootstraps i18next with additional language resources.
+export const loadDefault = async (lng: string) => {
   const resources: AnyJson = await Promise.all([
     await import(`./${lng}/pages.json`),
     await import(`./${lng}/help.json`),
--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -4,28 +4,56 @@
 import { DefaultLocale } from 'consts';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { AnyJson } from 'types';
 import { loadDefault } from './default';
+import helpEn from './en/help.json';
+import pagesEn from './en/pages.json';
+
+let lng: string;
+let dynamicLoad = false;
 
 const localLng = localStorage.getItem('lng');
-const lng = localLng ?? DefaultLocale;
 
+// determine the default language.
 if (!localLng) {
+  lng = DefaultLocale;
   localStorage.setItem('lng', DefaultLocale);
+} else {
+  lng = localLng;
+}
+
+// determine resources exist without dynamically importing them.
+let resources: AnyJson = null;
+if (lng === DefaultLocale) {
+  resources = {
+    en: {
+      ...pagesEn,
+      ...helpEn,
+    },
+  };
+} else {
+  // TODO: get entire language from localStorage if exists.
+  // TODO: introduce `lng_resources` localStorage item.
+  dynamicLoad = true;
 }
 
 // configure i18n object.
 i18next.use(initReactI18next).init({
   debug: process.env.REACT_APP_DEBUG_I18N === '1',
   fallbackLng: DefaultLocale,
-  lng,
+  lng: dynamicLoad ? 'en' : lng,
+  resources,
 });
 
-// load the default lanuage.
+// dynamically load default language resources if needed.
 (async () => {
+  if (!dynamicLoad) {
+    return;
+  }
   const {
     key,
     value: { pages, help },
-  } = await loadDefault();
+  } = await loadDefault(lng);
 
   i18next.addResourceBundle(key, 'pages', pages);
   i18next.addResourceBundle(key, 'help', help);
