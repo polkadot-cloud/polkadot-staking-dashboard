10da22df2f6801c7b2cc348f059b884b4ee043ad
Ross Bulat <ross@jkrbinvestments.com>
2022-11-18 12:18:32 +0000

    refactor to utils

--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -4,22 +4,15 @@
 import { AppVersion, DefaultLocale } from 'consts';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { AnyJson } from 'types';
 import baseEn from './en/base.json';
 import helpEn from './en/help.json';
-import { doDynamicImport } from './utils';
+import { doDynamicImport, getActiveLanguage, getResources } from './utils';
 
-// the supported namespaces
+// the supported namespaces.
 export const lngNamespaces = ['base', 'help'];
 
-// the active language
-let lng: string;
-
-// whether a dynamic import is needed
-let dynamicLoad = false;
-
-// default structure of language resources
-const resourcesInnerDefault = { ...baseEn, ...helpEn };
+// default structure of language resources.
+const fallbackResources = { ...baseEn, ...helpEn };
 
 // check app version, wipe `lng_resources` if version is different.
 const localAppVersion = localStorage.getItem('app_version');
@@ -32,51 +25,12 @@ if (localAppVersion !== AppVersion) {
   localStorage.setItem('app_version', AppVersion);
 }
 
-// determine the default language.
-const localLng = localStorage.getItem('lng');
-if (!localLng) {
-  lng = DefaultLocale;
-  localStorage.setItem('lng', DefaultLocale);
-} else {
-  lng = localLng;
-}
+// get active language.
+const lng: string = getActiveLanguage();
 
-// determine resources exist without dynamically importing them.
-let resources: AnyJson = null;
-if (lng === DefaultLocale) {
-  resources = {
-    en: resourcesInnerDefault,
-  };
-  localStorage.setItem(
-    'lng_resources',
-    JSON.stringify({ l: DefaultLocale, r: resourcesInnerDefault })
-  );
-} else {
-  // not the default locale, check if local resources exist
-  let localValid = false;
-  const localResources = localStorage.getItem('lng_resources');
-  if (localResources !== null) {
-    const { l, r } = JSON.parse(localResources);
-
-    if (l === lng) {
-      localValid = true;
-      // local resources found, load them in
-      resources = {
-        [lng]: {
-          ...r,
-        },
-      };
-    }
-  }
-
-  if (!localValid) {
-    // no resources exist locally, dynamic import needed.
-    dynamicLoad = true;
-    resources = {
-      en: resourcesInnerDefault,
-    };
-  }
-}
+// get default resources and whether a dynamic load is required for
+// the active language.
+const { resources, dynamicLoad } = getResources(lng, fallbackResources);
 
 // configure i18n object.
 i18next.use(initReactI18next).init({
--- src/locale/utils.tsx
+++ src/locale/utils.tsx
@@ -1,8 +1,72 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { DefaultLocale } from 'consts';
 import { AnyJson } from 'types';
 import { lngNamespaces } from './index';
+
+// Gets the active language
+//
+// Get the stored language from localStorage, or fallback to
+// DefaultLocale otherwise.
+export const getActiveLanguage = () => {
+  const localLng = localStorage.getItem('lng');
+  if (!localLng) {
+    localStorage.setItem('lng', DefaultLocale);
+    return DefaultLocale;
+  }
+  return localLng;
+};
+
+// Determine resources of selected language, and whether a dynamic
+// import is needed for missing language resources.
+//
+// If selected language is DefaultLocale, then we fall back to
+// the default language resources that have already been imported.
+export const getResources = (lng: string, fallbackResources: AnyJson) => {
+  let dynamicLoad = false;
+
+  let resources: AnyJson = null;
+  if (lng === DefaultLocale) {
+    // determine resources exist without dynamically importing them.
+    resources = {
+      en: fallbackResources,
+    };
+    localStorage.setItem(
+      'lng_resources',
+      JSON.stringify({ l: DefaultLocale, r: fallbackResources })
+    );
+  } else {
+    // not the default locale, check if local resources exist
+    let localValid = false;
+    const localResources = localStorage.getItem('lng_resources');
+    if (localResources !== null) {
+      const { l, r } = JSON.parse(localResources);
+
+      if (l === lng) {
+        localValid = true;
+        // local resources found, load them in
+        resources = {
+          [lng]: {
+            ...r,
+          },
+        };
+      }
+    }
+
+    if (!localValid) {
+      // no resources exist locally, dynamic import needed.
+      dynamicLoad = true;
+      resources = {
+        en: fallbackResources,
+      };
+    }
+  }
+  return {
+    resources,
+    dynamicLoad,
+  };
+};
 
 // Load language resources dynamically.
 //
