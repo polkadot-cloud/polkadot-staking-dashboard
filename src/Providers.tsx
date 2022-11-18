a12061b18e3b2069b2b53cb3a347d029293efe92
TingALin <linshaoting6@gmail.com>
2022-11-18 18:26:11 +0800

    appVersion abstraction

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from 'styled-components';
 import { EntryWrapper as Wrapper } from 'Wrappers';
 import { AccountProvider } from './contexts/Account';
 import { APIProvider, useApi } from './contexts/Api';
+import { AppVersionProvider } from './contexts/AppVersion';
 import { BalancesProvider } from './contexts/Balances';
 import { ConnectProvider } from './contexts/Connect';
 import { ExtrinsicsProvider } from './contexts/Extrinsics';
@@ -58,6 +59,7 @@ export const ThemedRouter = () => {
 };
 
 export const Providers = withProviders(
+  AppVersionProvider,
   APIProvider,
   ConnectProvider,
   HelpProvider,
