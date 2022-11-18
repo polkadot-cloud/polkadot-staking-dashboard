84d4b8071684847d9104a1416ec7641447a30121
Ross Bulat <ross@jkrbinvestments.com>
2022-11-18 12:03:56 +0000

    app_version & default -> utils

--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -1,13 +1,13 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { DefaultLocale } from 'consts';
+import { AppVersion, DefaultLocale } from 'consts';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { AnyJson } from 'types';
-import { loadLngAsync } from './default';
 import baseEn from './en/base.json';
 import helpEn from './en/help.json';
+import { loadLngAsync } from './utils';
 
 // the supported namespaces
 export const lngNamespaces = ['base', 'help'];
@@ -20,6 +20,17 @@ let dynamicLoad = false;
 
 // default structure of language resources
 const resourcesInnerDefault = { ...baseEn, ...helpEn };
+
+// check app version, wipe `lng_resources` if version is different.
+const localAppVersion = localStorage.getItem('app_version');
+if (localAppVersion !== AppVersion) {
+  localStorage.removeItem('lng_resources');
+  // localisation is currently the only feature that uses AppVersion.
+  // if more features require AppVersion in the future, this should be
+  // abstracted into a separate script that checks / updates AppVersion
+  // after any tidy up is completed.
+  localStorage.setItem('app_version', AppVersion);
+}
 
 // determine the default language.
 const localLng = localStorage.getItem('lng');
