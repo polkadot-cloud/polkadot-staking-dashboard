319f71fd82b69ad79ca1976c680952551260571f
Ross Bulat <ross@jkrbinvestments.com>
2022-11-20 16:44:19 +0000

    amend spacing

--- src/library/Overlay/Wrappers.tsx
+++ src/library/Overlay/Wrappers.tsx
@@ -68,7 +68,11 @@ export const ContentWrapper = styled.div`
     padding: 1rem 1rem 0 1rem;
   }
   .body {
-    padding: 1rem 1.5rem 0.75rem 1.5rem;
+    padding: 0.5rem 1.5rem 1rem 1.5rem;
+
+    h4 {
+      margin: 0.5rem 0;
+    }
   }
 `;
 
