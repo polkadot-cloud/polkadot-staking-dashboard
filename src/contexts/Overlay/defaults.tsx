be272d6ba34b9e340bfec5a949c8b910643356c6
Ross Bulat <ross@jkrbinvestments.com>
2022-11-21 11:44:49 +0000

    simplify lint disable

--- src/contexts/Overlay/defaults.tsx
+++ src/contexts/Overlay/defaults.tsx
@@ -4,12 +4,12 @@
 import { OverlayContextInterface } from './types';
 
 export const defaultOverlayContext: OverlayContextInterface = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   openOverlayWith: (o, s) => {},
   closeOverlay: () => {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   setStatus: (s) => {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   setOverlay: (d) => {},
   size: 'small',
   status: 0,
