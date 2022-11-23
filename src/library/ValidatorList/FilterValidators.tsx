f62d06b5422d09d14bb5c8bb026361a452cfd305
Ross Bulat <ross@jkrbinvestments.com>
2022-11-23 10:42:41 +0000

    map buttons

--- src/library/ValidatorList/FilterValidators.tsx
+++ src/library/ValidatorList/FilterValidators.tsx
@@ -12,85 +12,34 @@ export const FilterValidators = () => {
 
   const excludes = getExcludes('validators');
 
+  const filtersToLabels = {
+    inactive: 'Inactive Validators',
+    over_subscribed: 'Over Subscribed',
+    all_commission: '100% Commission',
+    blocked_nominations: 'Blocked Nominations',
+    missing_identity: 'Missing Identity',
+  };
+
   return (
     <FilterListWrapper>
       <Title title="Filter Validators" />
       <div className="body">
-        <button
-          type="button"
-          className="item"
-          onClick={() => {
-            toggleExclude('validators', 'inactive');
-          }}
-        >
-          <FontAwesomeIcon
-            transform="grow-2"
-            icon={excludes?.includes('inactive') ? faCheckCircle : faCircle}
-          />
-          <h4>Inactive Validators</h4>
-        </button>
-        <button
-          type="button"
-          className="item"
-          onClick={() => {
-            toggleExclude('validators', 'over_subscribed');
-          }}
-        >
-          <FontAwesomeIcon
-            transform="grow-2"
-            icon={
-              excludes?.includes('over_subscribed') ? faCheckCircle : faCircle
-            }
-          />
-          <h4>Over Subscribed</h4>
-        </button>
-        <button
-          type="button"
-          className="item"
-          onClick={() => {
-            toggleExclude('validators', 'all_commission');
-          }}
-        >
-          <FontAwesomeIcon
-            transform="grow-2"
-            icon={
-              excludes?.includes('all_commission') ? faCheckCircle : faCircle
-            }
-          />
-          <h4>100% Commission</h4>
-        </button>
-        <button
-          type="button"
-          className="item"
-          onClick={() => {
-            toggleExclude('validators', 'blocked_nominations');
-          }}
-        >
-          <FontAwesomeIcon
-            transform="grow-2"
-            icon={
-              excludes?.includes('blocked_nominations')
-                ? faCheckCircle
-                : faCircle
-            }
-          />
-          <h4>Blocked Nominations</h4>
-        </button>
-        <button
-          type="button"
-          className="item"
-          onClick={() => {
-            toggleExclude('validators', 'missing_identity');
-          }}
-        >
-          <FontAwesomeIcon
-            transform="grow-2"
-            icon={
-              excludes?.includes('missing_identity') ? faCheckCircle : faCircle
-            }
-          />
-          <h4>Missing Identity</h4>
-        </button>
+        {Object.entries(filtersToLabels).map(([f, l]: any, i: number) => (
+          <button
+            key={`validator_filter_${i}`}
+            type="button"
+            className="item"
+            onClick={() => {
+              toggleExclude('validators', f);
+            }}
+          >
+            <FontAwesomeIcon
+              transform="grow-2"
+              icon={excludes?.includes(f) ? faCheckCircle : faCircle}
+            />
+            <h4>{l}</h4>
+          </button>
+        ))}
       </div>
     </FilterListWrapper>
   );
