96f974dab6bc90b643af184040c20c64629b52d4
Ross Bulat <ross@jkrbinvestments.com>
2022-11-27 17:22:49 +0000

    Updates

---
+++ static/js/630.25986df7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpolkadot_staking_dashboard=self.webpackChunkpolkadot_staking_dashboard||[]).push([[630],{7630:(s,a,t)=>{t.r(a),t.d(a,{default:()=>d});var l=t(184);const d=()=>(0,l.jsxs)("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 64 64",children:[(0,l.jsx)("path",{style:{fill:"#45e1e5"},d:"M0 0h64v64H0z"}),(0,l.jsx)("path",{d:"M32 43a11 11 0 1 1 10.83-12.83h11.09a22 22 0 1 0 0 3.66H42.83A11 11 0 0 1 32 43Z",style:{fill:"#0a0b0d"}})]})}}]);
+//# sourceMappingURL=630.25986df7.chunk.js.map
