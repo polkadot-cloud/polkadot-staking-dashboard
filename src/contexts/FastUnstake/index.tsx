a65e15b8a18c67752ea3cb23cc06dae64a28716f
Ross Bulat <ross@jkrbinvestments.com>
2022-12-14 13:31:13 +0700

    + boilerplate

--- src/contexts/FastUnstake/index.tsx
+++ src/contexts/FastUnstake/index.tsx
@@ -4,8 +4,8 @@
 import { useApi } from 'contexts/Api';
 import { useConnect } from 'contexts/Connect';
 import { useNetworkMetrics } from 'contexts/Network';
-import React from 'react';
-import { AnyApi, MaybeAccount } from 'types';
+import React, { useState } from 'react';
+import { MaybeAccount } from 'types';
 // eslint-disable-next-line import/no-unresolved
 import Worker from 'worker-loader!../../workers/stakers';
 
@@ -26,35 +26,59 @@ export const FastUnstakeProvider = ({
   const { activeEra } = metrics;
   const { bondDuration } = consts;
 
+  // store whether a fast unstake check is in progress.
+  const [checking, setChecking] = useState<boolean>(false);
+
+  // TODO: store metadata relating to the fast unstake check.
+  // const [meta, setMeta] = useState<AnyJson>(null);
+
   // TODO: handle finished fast unstake eligibility check
   worker.onmessage = (message: MessageEvent) => {
-    console.log(message);
+    if (message) {
+      const { data } = message;
+      const { task } = data;
+      if (task !== 'fast_unstake_process_era') {
+        // TODO: implement callback
+      }
+    }
   };
 
   // initiate fast unstake eligibility check.
   const fastUnstakeEligible = async (a: MaybeAccount) => {
     // ensure current era has synced
-    if (activeEra.index === 0 || !bondDuration || !api || !a) return;
+    if (
+      activeEra.index === 0 ||
+      !bondDuration ||
+      !api ||
+      !a ||
+      checking ||
+      !activeAccount
+    )
+      return;
 
-    const calls = Array.from(Array(bondDuration).keys());
-    const exposuresRaw = await Promise.all(
-      calls.map((c: number) =>
-        api.query.staking.erasStakers.entries(metrics.activeEra.index - c)
-      )
-    );
+    setChecking(true);
 
-    // humanise exposures to send to worker
-    const exposures = exposuresRaw.map((e: AnyApi) =>
-      e.map(([_keys, _val]: AnyApi) => {
-        return {
-          keys: _keys.toHuman(),
-          val: _val.toHuman(),
-        };
-      })
-    );
+    // TODO: process one era at a time.
+    // const calls = Array.from(Array(bondDuration).keys());
+    // const exposuresRaw = await Promise.all(
+    //   calls.map((c: number) =>
+    //     api.query.staking.erasStakers.entries(metrics.activeEra.index - c)
+    //   )
+    // );
+
+    // // humanise exposures to send to worker
+    // const exposures = exposuresRaw.map((e: AnyApi) =>
+    //   e.map(([_keys, _val]: AnyApi) => {
+    //     return {
+    //       keys: _keys.toHuman(),
+    //       val: _val.toHuman(),
+    //     };
+    //   })
+    // );
 
     // send to worker to calculate eligibiltiy
     // worker.postMessage({
+    //   era,
     //   activeAccount,
     //   exposures,
     // });
