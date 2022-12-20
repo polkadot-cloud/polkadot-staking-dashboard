a584aabc24b7256ba76b64e05bc611e85fd770b7
Ting A Lin <linshaoting6@gmail.com>
2022-12-20 13:30:31 +0800

    Update MinimumNominatorBond.tsx

--- src/pages/Nominate/Active/Stats/MinimumNominatorBond.tsx
+++ src/pages/Nominate/Active/Stats/MinimumNominatorBond.tsx
@@ -4,15 +4,17 @@
 import { useApi } from 'contexts/Api';
 import { useStaking } from 'contexts/Staking';
 import { Number } from 'library/StatBoxList/Number';
+import { useTranslation } from 'react-i18next';
 import { planckBnToUnit } from 'Utils';
 
 export const MinimumNominatorBondStatBox = () => {
+  const { t } = useTranslation('pages');
   const { unit, units } = useApi().network;
   const { staking } = useStaking();
   const { minNominatorBond } = staking;
 
   const params = {
-    label: 'Minimum Nominate Bond',
+    label: t('nominate.minimumNominateBond')},
     value: planckBnToUnit(minNominatorBond, units),
     unit: `${unit}`,
     helpKey: 'Bonding',
