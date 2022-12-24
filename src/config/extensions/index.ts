66514d48995063c4fbd774818560a3337a5b8167
Ross Bulat <ross@jkrbinvestments.com>
2022-12-24 19:30:53 +0700

    bring back enkrypt

--- src/config/extensions/index.ts
+++ src/config/extensions/index.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ExtensionConfig } from 'contexts/Extensions/types';
+import { ReactComponent as EnkryptSVG } from './icons/enkrypt_icon.svg';
 import { ReactComponent as NovaWalletSVG } from './icons/nova_wallet.svg';
 import { ReactComponent as PolkadotJSSVG } from './icons/polkadot_js.svg';
 import { ReactComponent as SignerSVG } from './icons/signer_icon.svg';
@@ -19,14 +20,19 @@ export const EXTENSIONS: ExtensionConfig[] = [
       : PolkadotJSSVG,
   },
   {
-    id: 'subwallet-js',
-    title: 'SubWallet',
-    icon: SubwalletSVG,
-  },
-  {
     id: 'talisman',
     title: 'Talisman',
     icon: TalismanSVG,
+  },
+  {
+    id: 'enkrypt',
+    title: 'Enkrypt',
+    icon: EnkryptSVG,
+  },
+  {
+    id: 'subwallet-js',
+    title: 'SubWallet',
+    icon: SubwalletSVG,
   },
   {
     id: 'parity-signer-companion',
