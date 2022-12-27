e5d2299aedcad65e40403247bec18b392ff7f0c0
TingALin <linshaoting6@gmail.com>
2022-12-27 18:49:48 +0800

    cleanning

--- src/locale/check_order.js
+++ src/locale/check_order.js
@@ -1,23 +1,11 @@
 const fs = require('fs');
 const { join } = require('path');
 
-const getDeepKeys = (obj) => {
-    let keys = [];
-    for (const key in obj) {
-        keys.push(key);
-
-        if (typeof obj[key] === 'object') {
-            const subkeys = getDeepKeys(obj[key]);
-            keys = keys.concat(subkeys.map((subkey) => `${key}.${subkey}`));
-        }
-    }
-    return keys;
-};
 
 const enPath = join(__dirname, './en');
 const languages = fs.readdirSync(__dirname, { withFileTypes: true }).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
 
-const isAlphabaticOrder = () => {
+const orderKeys = () => {
     fs.readdir(enPath, (error, files) => {
         if (error) console.log(error);
         files.forEach((file) => {
@@ -26,28 +14,10 @@ const isAlphabaticOrder = () => {
                 const json = JSON.parse(
                     fs.readFileSync(join(languageFullPath, file)).toString()
                 );
-                //这里要分开各个file的keys
-                const keys = getDeepKeys(json);
-                // for (i in keys) {
-                //     // if (keys[i].length == 2) {
-                //     //     console.log(keys[i])
-                //     // }
-                //     console.log(keys[i].length)
-                // }
-
-
-                let l = keys.length;
-                for (let i = 1; i < l; i++) {
-                    if (keys[i] < keys[i - 1]) {
-                        return false;
-                    }
-                }
-                return true;
             })
         }
         );
     });
 };
 
-if (isAlphabaticOrder()) { console.log("It is alphabaticlly ordered"); }
-else { console.log("It is NOT alphabaticlly ordered") }
+orderKeys()
