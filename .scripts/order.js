9d0e7ba91507bcfda5f87e154800775f6833ba27
Ross Bulat <ross@parity.io>
2022-12-28 22:27:53 +0700

    lint tidy-up + lint

--- .scripts/order.js
+++ .scripts/order.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const prettier = require("prettier");
+const prettier = require('prettier');
 
 const languageDir = path.join(__dirname, '..', 'src', 'locale');
 
@@ -61,21 +61,26 @@ for (const lng of languages) {
     }
     files.forEach((file) => {
       const pathToFile = path.join(pathToLanguage, file);
-     
+
       // get file in raw JSOn
-      const json = JSON.parse(
-        fs.readFileSync(pathToFile).toString()
-      );
+      const json = JSON.parse(fs.readFileSync(pathToFile).toString());
 
       // order json object alphabetically
       const orderedJson = orderKeys(json);
 
       // write the updated JSON as a string back into file.
-      fs.writeFile(pathToFile,  prettier.format(JSON.stringify({...orderedJson}), { parser: "json" }), function (err) {
-
-        if (err) { console.err(err); }
-        console.log(`----------Keys In ${pathToLanguage}/${file} Are Ordered Alphabetically-------------`);
-      });
+      fs.writeFile(
+        pathToFile,
+        prettier.format(JSON.stringify({ ...orderedJson }), { parser: 'json' }),
+        function (err) {
+          if (err) {
+            console.err(err);
+          }
+          console.log(
+            `----------Keys In ${pathToLanguage}/${file} Are Ordered Alphabetically-------------`
+          );
+        }
+      );
     });
   });
 }
