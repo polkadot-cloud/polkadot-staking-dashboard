95fcd98f6a1490256a3f6f54c5690b210318f4f3
Ross Bulat <ross@parity.io>
2023-01-02 18:58:05 +0700

    license

--- tests/graphs.test.ts
+++ tests/graphs.test.ts
@@ -1,4 +1,5 @@
-// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+/* Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+ * SPDX-License-Identifier: Apache-2.0 */
 
 const sum = (a: any, b: any) => a + b;
 
