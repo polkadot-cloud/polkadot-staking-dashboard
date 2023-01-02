530c746f5a87998c6d80e013b7a022b6301f197e
TingALin <linshaoting6@gmail.com>
2023-01-02 16:01:10 +0800

    fixes

--- src/library/StatBoxList/Timeleft.tsx
+++ src/library/StatBoxList/Timeleft.tsx
@@ -6,7 +6,7 @@ import { StatPie } from 'library/Graphs/StatBoxPie';
 import { OpenHelpIcon } from 'library/OpenHelpIcon';
 import { StatBox } from './Item';
 import { TimeleftProps } from './types';
-import { TineleftWrapper } from './Wrapper';
+import { TimeleftWrapper } from './Wrapper';
 
 export const Timeleft = (props: TimeleftProps) => {
   const { label, timeleft, graph, tooltip, helpKey } = props;
@@ -25,9 +25,9 @@ export const Timeleft = (props: TimeleftProps) => {
         </div>
 
         <div className="labels">
-          <TineleftWrapper>
+          <TimeleftWrapper>
             <Countdown timeleft={timeleft} />
-          </TineleftWrapper>
+          </TimeleftWrapper>
           <h4>
             {label} {help && <OpenHelpIcon helpKey={helpKey} />}
           </h4>
