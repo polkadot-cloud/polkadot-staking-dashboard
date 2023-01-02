cf9c5298afdd8c80a91479fddc608f60ab4ae5d8
Ross Bulat <ross@parity.io>
2023-01-02 18:55:56 +0700

    + jest setup

---
+++ tests/graphs.test.ts
@@ -0,0 +1,9 @@
+// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+
+const sum = (a: any, b: any) => a + b;
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+export {};
