4a374417a19bb4608b9e0505430ad8055adc5289
Ross Bulat <ross@parity.io>
2023-01-04 16:50:23 +0700

    rename

--- src/library/Hooks/useErasToTimeleft/index.tsx
+++ src/library/Hooks/useErasToTimeleft/index.tsx
@@ -3,7 +3,7 @@
 
 import { useApi } from 'contexts/Api';
 
-export const useErasToTimeleft = () => {
+export const useErasToTimeLeft = () => {
   const { consts } = useApi();
   const { epochDuration, expectedBlockTime, sessionsPerEra } = consts;
 
