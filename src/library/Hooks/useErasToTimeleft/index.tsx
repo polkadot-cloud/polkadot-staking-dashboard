7c0a9f081d409d5ce768326f981e259e5c14adfd
Ross Bulat <ross@parity.io>
2023-01-04 15:26:33 +0700

    return 0 if 0 eras

--- src/library/Hooks/useErasToTimeleft/index.tsx
+++ src/library/Hooks/useErasToTimeleft/index.tsx
@@ -7,11 +7,16 @@ export const useErasToTimeleft = () => {
   const { consts } = useApi();
   const { epochDuration, expectedBlockTime, sessionsPerEra } = consts;
 
+  // converts a number of eras to timeleft in seconds.
   const erasToTimeLeft = (eras: number) => {
-    // store the duration of an era in block numbers.
+    if (!eras) {
+      return 0;
+    }
+    // store the duration of an era in number of blocks.
     const eraDurationBlocks = epochDuration * sessionsPerEra;
-    // estimate the duration of the era in seconds
+    // estimate the duration of the era in seconds.
     const eraDuration = eraDurationBlocks * expectedBlockTime * 0.001;
+    // multiply by number of eras.
     return eras * eraDuration;
   };
 
