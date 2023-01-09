dd38c23cf09c98de026168bd12089f82c3c3202e
Ross Bulat <ross@parity.io>
2023-01-09 14:13:40 +0700

    expand syncing support

--- src/pages/Overview/StakeStatus/index.tsx
+++ src/pages/Overview/StakeStatus/index.tsx
@@ -13,7 +13,7 @@ import { useUi } from 'contexts/UI';
 import { getUnixTime } from 'date-fns';
 import { CardWrapper } from 'library/Graphs/Wrappers';
 import { useNominationStatus } from 'library/Hooks/useNominationStatus';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { determinePoolDisplay } from 'Utils';
 import { Item } from './Item';
@@ -23,7 +23,15 @@ import { StatusWrapper } from './Wrappers';
 export const StakeStatus = () => {
   const { network } = useApi();
   const navigate = useNavigate();
-  const { networkSyncing, isSyncing } = useUi();
+  const {
+    networkSyncing,
+    isSyncing,
+    getSyncById,
+    getSyncStart,
+    setSyncStart,
+    getSyncSynced,
+    setSyncSynced,
+  } = useUi();
   const { activeAccount } = useConnect();
   const { openModalWith } = useModal();
   const { membership } = usePoolMemberships();
@@ -35,30 +43,42 @@ export const StakeStatus = () => {
   const isStaking = isNominating() || membership;
   const showTips = plugins.includes('tips');
 
+  // sync start id
+  const syncId = `${JSON.stringify(activeAccount)}_${network.name}`;
+
   // delay refresh to avoid flashing updates.
   const delay = 1000;
 
-  // state to control min wait time for syncing.
-  const [syncStart, setSyncStart] = useState<number>(0);
-
-  const [syncing, setSyncing] = useState<boolean>(true);
-
-  // update status content after time.
   useEffect(() => {
-    const remaining = Math.max(0, syncStart + delay - getUnixTime(new Date()));
-    if (remaining > 0) {
-      setTimeout(() => {
-        setSyncing(false);
-      }, remaining);
-    } else {
-      setSyncing(false);
+    const synced = getSyncSynced(syncId);
+    const syncStart = getSyncStart(syncId);
+
+    // if syncing, get remaining time.
+    if (!synced) {
+      const remaining = Math.max(
+        0,
+        syncStart + delay - getUnixTime(new Date())
+      );
+
+      if (remaining > 0) {
+        setTimeout(() => {
+          // console.log('remaining ', remaining);
+          setSyncSynced(syncId);
+        }, remaining);
+      } else {
+        // console.log('synced');
+        setSyncSynced(syncId);
+      }
     }
-  }, [syncStart]);
+  }, [getSyncStart(syncId), getSyncSynced(syncId)]);
 
   // re-sync: set sync start time
   useEffect(() => {
-    setSyncing(true);
-    setSyncStart(getUnixTime(new Date()));
+    // this is firing when we switch tabs AND when network is switched...
+
+    if (getSyncById(syncId) === null) {
+      setSyncStart(syncId, getUnixTime(new Date()));
+    }
   }, [network, activeAccount]);
 
   const poolDisplay = () => {
@@ -84,7 +104,7 @@ export const StakeStatus = () => {
   return (
     <CardWrapper>
       <StatusWrapper includeBorder={showTips}>
-        {networkSyncing || syncing ? (
+        {networkSyncing || !getSyncSynced(syncId) ? (
           <Item
             leftIcon={{ show: true, active: false }}
             text="Syncing Status..."
