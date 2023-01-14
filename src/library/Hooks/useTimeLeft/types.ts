6d84a5ff586b57d824c9647b851f4f92e72f24fd
Ross Bulat <ross@parity.io>
2023-01-14 13:09:28 +0700

    update copyright

--- src/library/Hooks/useTimeLeft/types.ts
+++ src/library/Hooks/useTimeLeft/types.ts
@@ -1,4 +1,4 @@
-// Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
+// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 
 export interface TimeleftDuration {
   days: number;
