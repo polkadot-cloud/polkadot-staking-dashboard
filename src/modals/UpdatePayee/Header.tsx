4c05a7f5f4d4c3e1d6ef4cf5b99a76962612153c
Ross Bulat <ross@parity.io>
2023-01-31 14:44:12 +0700

    use title

--- src/modals/UpdatePayee/Header.tsx
+++ src/modals/UpdatePayee/Header.tsx
@@ -3,19 +3,30 @@
 
 import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PayeeItem, usePayeeConfig } from 'library/Hooks/usePayeeConfig';
 import { HeaderWrapper } from './Wrappers';
 
 export const Header = ({ current, selected }: any) => {
+  const { getPayeeItems } = usePayeeConfig();
+
+  const currentTitle =
+    getPayeeItems(true).find((p: PayeeItem) => p.value === current)?.title ||
+    '';
+
+  const selectedTitle =
+    getPayeeItems(true).find((p: PayeeItem) => p.value === selected)?.title ||
+    '';
+
   return (
     <HeaderWrapper>
       <div>
-        <h4>{current ?? ''}</h4>
+        <h4>{currentTitle}</h4>
       </div>
       <span>
         <FontAwesomeIcon icon={faAnglesRight} />
       </span>
       <div>
-        <h4>{selected || ''}</h4>
+        <h4>{selectedTitle}</h4>
       </div>
     </HeaderWrapper>
   );
