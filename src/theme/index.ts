4e7d3951ce78d91fd12a499beba86bd68796d903
Ross Bulat <ross@parity.io>
2023-02-09 14:10:27 +0800

    migrate tooltip

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,17 +1,6 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import theme from 'styled-theming';
-import { defaultThemes } from './default';
 
 /* Aggregates all theme configurations and serves the currently
  * active mode via the theming context.
  */
-const v = 'mode';
-
-// graphs
-
-export const tooltipBackground: theme.ThemeSet = theme(
-  v,
-  defaultThemes.graphs.tooltip
-);
+export const v = 'mode';
