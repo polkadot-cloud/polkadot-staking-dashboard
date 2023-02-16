cea33b686b7f673fcc47d194ce759b250e8a5602
cuteolaf <oliverlim818@gmail.com>
2023-02-15 19:05:16 -0800

    update investors page

--- src/pages/Investors/ManageFund.tsx
+++ src/pages/Investors/ManageFund.tsx
@@ -1,5 +1,6 @@
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { ButtonPrimary } from '@rossbulat/polkadot-dashboard-ui';
+import { useAccount } from 'contexts/Account';
 import { useModal } from 'contexts/Modal';
 import { InvestGraph } from 'library/Graphs/Invest';
 import { CardHeaderWrapper } from 'library/Graphs/Wrappers';
@@ -9,6 +10,7 @@ import { ButtonRowWrapper } from 'Wrappers';
 export const ManageFund = () => {
   const { t } = useTranslation('pages');
   const { openModalWith } = useModal();
+  const { isInvestor } = useAccount();
 
   const onWithdraw = () => {
     openModalWith('WithdrawFund', {});
@@ -28,11 +30,13 @@ export const ManageFund = () => {
           iconLeft={faPlus}
           onClick={onDeposit}
           marginRight
+          disabled={!isInvestor()}
         />
         <ButtonPrimary
           text={t('investors.withdraw')}
           iconLeft={faMinus}
           onClick={onWithdraw}
+          disabled={!isInvestor()}
         />
       </ButtonRowWrapper>
       <InvestGraph />
