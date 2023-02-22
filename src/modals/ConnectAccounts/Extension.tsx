01ddeb1f5205f8059db6386d78e107f0466161ee
marshacb <cameron.marshall12@gmail.com>
2023-02-22 13:04:42 -0500

    add wallet connect universal provider

--- src/modals/ConnectAccounts/Extension.tsx
+++ src/modals/ConnectAccounts/Extension.tsx
@@ -1,8 +1,13 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import SignClient from '@walletconnect/sign-client';
-import { Web3Modal } from '@web3modal/standalone';
+import UniversalProvider from '@walletconnect/universal-provider';
+
+// import { Core } from '@walletconnect/core';
+// import { Web3Wallet } from '@walletconnect/web3wallet';
+
+// import SignClient from '@walletconnect/sign-client';
+// import { Web3Modal } from '@web3modal/standalone';
 
 import { faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -61,7 +66,7 @@ export const Extension = (props: ExtensionProps) => {
 
 export const ExtensionButton = (props: any) => {
   const { meta, setSection, installed } = props;
-  const { status, id } = meta;
+  const { status, id: extensionId } = meta;
 
   const { connectExtensionAccounts } = useConnect();
 
@@ -70,34 +75,74 @@ export const ExtensionButton = (props: any) => {
 
   // click to connect to extension
   const handleClick = async () => {
-    console.log('id', id);
+    console.log('id', extensionId);
     if (status === 'connected') {
       setSection(1);
-    } else if (id === 'wallet-connect') {
+    } else if (extensionId === 'wallet-connect') {
       console.log('this is wallet connect handler');
       const projectId = 'f75434b01141677e4ee7ddf70fee56b4';
-      //  'polkadot:91b171bb158e2d3848fa23a9f1c25182'
-      const web3Modal = new Web3Modal({
-        walletConnectVersion: 1, // or 2
-        projectId,
-        standaloneChains: ['eip155:43114'],
-      });
-      const signClient = await SignClient.init({ projectId });
 
-      const { uri, approval } = await signClient.connect({
-        requiredNamespaces: {
-          eip155: {
-            methods: ['eth_sign'],
-            chains: ['eip155:43114'],
-            events: ['accountsChanged'],
+      const provider = await UniversalProvider.init({
+        projectId,
+        metadata: {
+          name: 'Polkadot Staking Dashboard',
+          description: 'dApp for Staking Nomination Pool Staking',
+          url: 'https://https://staking.polkadot.network/',
+          icons: ['../../../public/favicons/polkadot/favicon-16x16.png'],
+        },
+      });
+
+      console.log('what is the provider---', provider);
+
+      // Subscribe for pairing URI
+      provider.on('display_uri', async (uri: string) => {
+        console.log('display_uri', uri);
+      });
+
+      // Subscribe to session ping
+      provider.on(
+        'session_ping',
+        ({ id, topic }: { id: string; topic: string }) => {
+          console.log(id, topic);
+        }
+      );
+
+      // Subscribe to session event
+      provider.on(
+        'session_event',
+        ({ event, chainId }: { event: any; chainId: any }) => {
+          console.log(event, chainId);
+        }
+      );
+
+      // Subscribe to session update
+      provider.on(
+        'session_update',
+        ({ topic, params }: { topic: any; params: any }) => {
+          console.log(topic, params);
+        }
+      );
+
+      // Subscribe to session delete
+      provider.on(
+        'session_delete',
+        ({ id, topic }: { id: string; topic: string }) => {
+          console.log(id, topic);
+        }
+      );
+
+      await provider.connect({
+        namespaces: {
+          polkadot: {
+            methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
+            chains: ['polkadot:91b171bb158e2d3848fa23a9f1c25182'],
+            events: ['chainChanged", "accountsChanged'],
+            rpcMap: {
+              '91b171bb158e2d3848fa23a9f1c25182': 'wss://rpc.polkadot.io', // your rpc url,
+            },
           },
         },
       });
-      if (uri) {
-        web3Modal.openModal({ uri, standaloneChains: ['eip155:43114'] });
-        await approval();
-        web3Modal.closeModal();
-      }
     } else {
       (() => {
         connectExtensionAccounts(installed);
