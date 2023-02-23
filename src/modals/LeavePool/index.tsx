f3ab54af20585d742470056ab3cd71e7f7dc755e
Ross Bulat <ross@parity.io>
2023-02-23 10:33:37 +0700

    timeleftAsString takes start time

--- src/modals/LeavePool/index.tsx
+++ src/modals/LeavePool/index.tsx
@@ -10,6 +10,7 @@ import { useModal } from 'contexts/Modal';
 import { useActivePools } from 'contexts/Pools/ActivePools';
 import { useTransferOptions } from 'contexts/TransferOptions';
 import { useTxFees } from 'contexts/TxFees';
+import { getUnixTime } from 'date-fns';
 import { Warning } from 'library/Form/Warning';
 import { useErasToTimeLeft } from 'library/Hooks/useErasToTimeLeft';
 import { useSubmitExtrinsic } from 'library/Hooks/useSubmitExtrinsic';
@@ -40,6 +41,7 @@ export const LeavePool = () => {
 
   const bondDurationFormatted = timeleftAsString(
     t,
+    getUnixTime(new Date()) + 1,
     erasToSeconds(bondDuration),
     true
   );
