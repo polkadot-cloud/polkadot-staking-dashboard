90bdcc558be04be2a1b73b37da033a8d99529988
Ross Bulat <ross@parity.io>
2023-02-24 23:15:08 +0700

    plus -> minus

--- src/contexts/Subscan/index.tsx
+++ src/contexts/Subscan/index.tsx
@@ -113,7 +113,7 @@ export const SubscanProvider = ({
           unclaimedList.forEach((p: AnyApi) => {
             p.block_timestamp = activeEra.start
               .multipliedBy(0.001)
-              .minus(erasToSeconds(activeEra.index.minus(p.era).plus(1)))
+              .minus(erasToSeconds(activeEra.index.minus(p.era).minus(1)))
               .toNumber();
           });
           newUnclaimedPayouts = newUnclaimedPayouts.concat(unclaimedList);
