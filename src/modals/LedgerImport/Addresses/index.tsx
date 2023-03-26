1bcad4e4443ba6f0c523b37e92415707ad6fd720
Ross Bulat <ross@parity.io>
2023-03-26 12:17:01 +0700

    add kusama ledger app icon

--- src/modals/LedgerImport/Addresses/index.tsx
+++ src/modals/LedgerImport/Addresses/index.tsx
@@ -3,8 +3,11 @@
 
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { ButtonText } from '@polkadotcloud/dashboard-ui';
+import { capitalizeFirstLetter } from 'Utils';
+import { useApi } from 'contexts/Api';
 import { useLedgerHardware } from 'contexts/Hardware/Ledger';
-import { ReactComponent as AppSVG } from 'img/appIcons/polkadot.svg';
+import { ReactComponent as AppSVGKusama } from 'img/appIcons/kusama.svg';
+import { ReactComponent as AppSVGPolkadot } from 'img/appIcons/polkadot.svg';
 import { useTranslation } from 'react-i18next';
 import type { AnyJson } from 'types';
 import { Address } from './Address';
@@ -12,6 +15,7 @@ import { AddressWrapper } from './Wrappers';
 
 export const Addresess = ({ addresses, handleLedgerLoop }: AnyJson) => {
   const { t } = useTranslation('modals');
+  const { name } = useApi().network;
   const { getIsExecuting, setIsExecuting, pairDevice } = useLedgerHardware();
   const isExecuting = getIsExecuting();
   return (
@@ -20,8 +24,9 @@ export const Addresess = ({ addresses, handleLedgerLoop }: AnyJson) => {
         <AddressWrapper>
           <div className="heading">
             <h4>
-              <AppSVG />
-              <span>Polkadot</span>
+              {name === 'polkadot' && <AppSVGPolkadot />}
+              {name === 'kusama' && <AppSVGKusama />}
+              <span>{capitalizeFirstLetter(name)}</span>
             </h4>
           </div>
           <div className="items">
