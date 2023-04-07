d249cf00477a64443e7337d31c932afb9f2abd7b
Ross Bulat <ross@parity.io>
2023-04-07 15:13:08 +0700

    fix h4 styles

--- src/library/StatusLabel/Wrapper.ts
+++ src/library/StatusLabel/Wrapper.ts
@@ -28,7 +28,6 @@ export const Wrapper = styled.div<WrapperProps>`
       color: var(--text-color-secondary);
     }
     h2 {
-      padding: 0;
       display: flex;
       flex-flow: row wrap;
       align-items: center;
