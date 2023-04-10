eeda9d3ea453df6fe2360799b2b329afa7bf9561
Ross Bulat <ross@parity.io>
2023-04-10 14:10:50 +0700

    sync to dot lottie

--- src/pages/Overview/StakeStatus/Tips/Syncing.tsx
+++ src/pages/Overview/StakeStatus/Tips/Syncing.tsx
@@ -1,22 +1,13 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import refreshChangeJson from 'img/json/refresh-change-outline.json';
+import { useDotLottieButton } from 'library/Hooks/useDotLottieButton';
 import { useTranslation } from 'react-i18next';
-import Lottie from 'react-lottie';
-import { ItemInnerWrapper, ItemsWrapper, ItemWrapper } from './Wrappers';
+import { ItemInnerWrapper, ItemWrapper, ItemsWrapper } from './Wrappers';
 
 export const Syncing = () => {
   const { t } = useTranslation('tips');
-
-  const animateOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: refreshChangeJson,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+  const { icon } = useDotLottieButton('refresh', {}, { autoLoop: true });
 
   return (
     <ItemsWrapper
@@ -31,14 +22,10 @@ export const Syncing = () => {
     >
       <ItemWrapper>
         <ItemInnerWrapper inactive>
-          <section style={{ paddingRight: '0.5rem' }}>
-            <Lottie
-              options={animateOptions}
-              width="1.6rem"
-              height="1.6rem"
-              isStopped={false}
-              isPaused={false}
-            />
+          <section
+            style={{ paddingRight: '0.5rem', width: '2rem', height: '2rem' }}
+          >
+            {icon}
           </section>
           <section>
             <div className="desc">
