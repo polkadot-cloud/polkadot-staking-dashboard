98db2f68a18267c2606a00733d099c79ed5dbfee
TingALin <linshaoting6@gmail.com>
2023-04-11 23:00:13 +0800

    rm useTabs

--- src/contexts/Tabs/types.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export interface TabsContextInterface {
-  setActiveTab: (t: number) => void;
-  activeTab: number;
-}
