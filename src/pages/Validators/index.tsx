7f97ed68f9e942cb0166b60f716218e7a7995ce3
Ross Bulat <ross@parity.io>
2023-04-12 20:01:26 +0700

    separate all tabs into own components

--- src/pages/Validators/index.tsx
+++ src/pages/Validators/index.tsx
@@ -1,30 +1,16 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { PageRow } from '@polkadotcloud/core-ui';
-import { useApi } from 'contexts/Api';
-import { useValidators } from 'contexts/Validators';
-import { CardWrapper } from 'library/Graphs/Wrappers';
 import { PageTitle } from 'library/PageTitle';
-import { StatBoxList } from 'library/StatBoxList';
-import { ValidatorList } from 'library/ValidatorList';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { AllValidators } from './AllValidators';
 import { ValidatorFavorites } from './Favorites';
-import { ActiveValidatorsStat } from './Stats/ActiveValidators';
-import { AverageCommissionStat } from './Stats/AverageCommission';
-import { TotalValidatorsStat } from './Stats/TotalValidators';
 import { ValidatorsTabsProvider, useValidatorsTabs } from './context';
 
 export const ValidatorsInner = () => {
   const { t } = useTranslation('pages');
-  const { isReady } = useApi();
-  const { validators } = useValidators();
   const { activeTab, setActiveTab } = useValidatorsTabs();
-  const defaultFilters = {
-    includes: ['active'],
-    excludes: ['all_commission', 'blocked_nominations', 'missing_identity'],
-  };
 
   // back to tab 0 if not in the first tab
   useEffect(() => {
@@ -50,48 +36,7 @@ export const ValidatorsInner = () => {
   return (
     <>
       <PageTitle title={t('validators.validators')} tabs={tabs} />
-      {activeTab === 0 && (
-        <>
-          <StatBoxList>
-            <ActiveValidatorsStat />
-            <TotalValidatorsStat />
-            <AverageCommissionStat />
-          </StatBoxList>
-          <PageRow>
-            <CardWrapper>
-              {!isReady ? (
-                <div className="item">
-                  <h3>{t('validators.connecting')}...</h3>
-                </div>
-              ) : (
-                <>
-                  {validators.length === 0 && (
-                    <div className="item">
-                      <h3>{t('validators.fetchingValidators')}...</h3>
-                    </div>
-                  )}
-
-                  {validators.length > 0 && (
-                    <ValidatorList
-                      bondFor="nominator"
-                      validators={validators}
-                      batchKey="validators_browse"
-                      title={t('validators.networkValidators')}
-                      selectable={false}
-                      defaultFilters={defaultFilters}
-                      allowMoreCols
-                      allowFilters
-                      allowSearch
-                      pagination
-                      toggleFavorites
-                    />
-                  )}
-                </>
-              )}
-            </CardWrapper>
-          </PageRow>
-        </>
-      )}
+      {activeTab === 0 && <AllValidators />}
       {activeTab === 1 && <ValidatorFavorites />}
     </>
   );
