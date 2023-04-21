8b2a113ab5df98bbe3e57d1af2cbb495674b28f9
Ross Bulat <ross@parity.io>
2023-04-21 20:21:07 +0800

    only display delegates that are imported

--- src/modals/Accounts/Delegates.tsx
+++ src/modals/Accounts/Delegates.tsx
@@ -3,13 +3,22 @@
 
 import type { ProxyDelegate } from 'contexts/Accounts/Proxies/type';
 import { useConnect } from 'contexts/Connect';
+import type { ImportedAccount } from 'contexts/Connect/types';
 import { AccountButton } from './Account';
 import { AccountGroupWrapper } from './Wrappers';
 import type { DelegatesProps } from './types';
 
 export const Delegates = ({ delegates, delegator }: DelegatesProps) => {
-  const { getAccount } = useConnect();
-  const delegatesList = delegates?.delegates || [];
+  const { accounts, getAccount } = useConnect();
+
+  // Remove delegates that are not imported accounts. Default to empty array if there are no
+  // delegates for this address.
+  const delegatesList =
+    delegates?.delegates.filter(
+      (d: ProxyDelegate) =>
+        accounts.find((a: ImportedAccount) => a.address === d.delegate) !==
+        undefined
+    ) || [];
 
   return (
     <>
