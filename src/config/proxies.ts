cc5bba83a595c8a45bdb5f534adf3903204c044a
Ross Bulat <ross@parity.io>
2023-05-16 17:32:22 +0700

    add UnsupportedIfUniqueController array

--- src/config/proxies.ts
+++ src/config/proxies.ts
@@ -16,6 +16,15 @@ export const SupportedProxies: Record<string, string[]> = {
   ],
 };
 
+export const UnsupportedIfUniqueController: string[] = [
+  'staking.chill',
+  'staking.nominate',
+  'staking.rebond',
+  'staking.setPayee',
+  'staking.unbond',
+  'staking.withdrawUnbonded',
+];
+
 export const isSupportedProxy = (proxy: string) =>
   Object.keys(SupportedProxies).includes(proxy) || proxy === 'Any';
 
