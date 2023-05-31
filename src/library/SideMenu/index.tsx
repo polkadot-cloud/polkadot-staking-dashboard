5dfaabf56454fa33965d5478c5c41efe2e7fb5f8
Ada <>
2023-05-31 16:05:06 -0400

    Remove Chinese language option

--- src/library/SideMenu/index.tsx
+++ src/library/SideMenu/index.tsx
@@ -142,12 +142,6 @@ export const SideMenu = () => {
         >
           <CogOutlineSVG width="1.3em" height="1.3em" />
         </button>
-        <button
-          type="button"
-          onClick={() => openModalWith('ChooseLanguage', {}, 'small')}
-        >
-          <LanguageSVG width="1.25em" height="1.25em" />
-        </button>
       </section>
     </Wrapper>
   );
