edd27f619c2d37a9159094cb065e1bcd6bc89501
Ada <>
2023-05-31 12:09:45 -0400

    Fix background color; Update Github link to point to creditcoin dashboard

--- src/library/SideMenu/index.tsx
+++ src/library/SideMenu/index.tsx
@@ -129,7 +129,7 @@ export const SideMenu = () => {
           type="button"
           onClick={() =>
             window.open(
-              'https://github.com/paritytech/polkadot-staking-dashboard',
+              'https://github.com/gluwa/creditcoin-staking-dashboard',
               '_blank'
             )
           }
