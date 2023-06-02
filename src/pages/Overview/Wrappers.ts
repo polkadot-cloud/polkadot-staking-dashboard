33729791933331fa21d9bf0a31fcc920dcbcbb88
Ross Bulat <ross@parity.io>
2023-06-02 14:20:01 +0700

    add banner

--- src/pages/Overview/Wrappers.ts
+++ src/pages/Overview/Wrappers.ts
@@ -18,6 +18,7 @@ export const MoreWrapper = styled.div`
   display: flex;
   flex-flow: column wrap;
   margin-top: 3.5rem;
+
   @media (max-width: ${SectionFullWidthThreshold}px) {
     margin-top: 1rem;
     margin-bottom: 0.5rem;
@@ -29,5 +30,70 @@ export const MoreWrapper = styled.div`
   section {
     width: 100%;
     margin-top: 0.1rem;
+  }
+`;
+
+export const BannerWrapper = styled.div`
+  &.light {
+    border: 1px solid var(--network-color-primary);
+    background: var(--background-primary);
+
+    .label,
+    > div h3 {
+      color: var(--network-color-primary);
+    }
+  }
+  &.dark {
+    border: 1px solid var(--border-secondary-color);
+    background: var(--network-color-secondary);
+
+    .label,
+    > div h3 {
+      color: white;
+    }
+    div > button {
+      color: white;
+      border-color: white;
+    }
+  }
+  box-shadow: var(--card-shadow-secondary);
+  border-radius: 1.25rem;
+  padding: 1.25rem 1.25rem;
+  margin-top: 6rem;
+  width: 100%;
+
+  .label {
+    color: white;
+    margin-bottom: 0.75rem;
+
+    .icon {
+      margin-right: 0.35rem;
+    }
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      font-family: InterSemiBold, sans-serif;
+      line-height: 1.8rem;
+    }
+    button {
+      flex-basis: auto;
+      font-size: 1.1rem;
+      margin-left: 0.75rem;
+    }
+    @media (max-width: 800px) {
+      flex-direction: column;
+      align-items: flex-start;
+
+      button {
+        margin-top: 0.75rem;
+        margin-left: 0;
+        position: relative;
+        left: -0.5rem;
+      }
+    }
   }
 `;
