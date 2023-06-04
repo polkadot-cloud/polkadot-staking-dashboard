bc399d461323ddf356f0061dbdac0ab14469271d
Ross Bulat <ross@parity.io>
2023-06-04 15:17:35 +0700

    rm derecated consts

--- src/consts.ts
+++ src/consts.ts
@@ -20,7 +20,6 @@ export const EmptyH256 = new Uint8Array(32);
 export const ModPrefix = stringToU8a('modl');
 export const U32Opts = { bitLength: 32, isLe: true };
 
-export const InterfaceMaximumWidth = 1500;
 export const SideMenuMaximisedWidth = 185;
 export const SideMenuMinimisedWidth = 75;
 export const SideMenuStickyThreshold = 1175;
@@ -28,7 +27,6 @@ export const SectionFullWidthThreshold = 1050;
 export const ShowAccountsButtonWidthThreshold = 850;
 export const FloatingMenuWidth = 250;
 export const SmallFontSizeMaxWidth = 600;
-export const MediumFontSizeMaxWidth = 1600;
 export const TipsThresholdSmall = 750;
 export const TipsThresholdMedium = 1200;
 
