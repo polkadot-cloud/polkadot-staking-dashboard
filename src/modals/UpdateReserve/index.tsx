c7bab60c6225a6871fbdd1ba7552ffcaf85a2f24
TingALin <linshaoting6@gmail.com>
2023-06-06 11:10:36 +0700

    add close & help button

--- src/modals/UpdateReserve/index.tsx
+++ src/modals/UpdateReserve/index.tsx
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {
+  ActionItem,
+  ButtonHelp,
   ButtonPrimaryInvert,
   ModalPadding,
   ModalWarnings,
@@ -11,9 +13,11 @@ import BigNumber from 'bignumber.js';
 import { useApi } from 'contexts/Api';
 import { useBalances } from 'contexts/Balances';
 import { useConnect } from 'contexts/Connect';
+import { useHelp } from 'contexts/Help';
 import { useModal } from 'contexts/Modal';
 import { useTransferOptions } from 'contexts/TransferOptions';
 import { Warning } from 'library/Form/Warning';
+import { Close } from 'library/Modal/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import React from 'react';
@@ -31,6 +35,7 @@ export const UpdateReserve = () => {
   const { setStatus } = useModal();
   const allTransferOptions = getTransferOptions(activeAccount);
   const { frozen } = balance;
+  const { openHelp } = useHelp();
 
   // check account non-staking locks
   const locks = getLocks(activeAccount);
@@ -38,6 +43,7 @@ export const UpdateReserve = () => {
   const lockStakingAmount = locksStaking
     ? locksStaking.amount
     : new BigNumber(0);
+  // available balance data
   const fundsFree = planckToUnit(
     allTransferOptions.freeBalance.minus(frozen.minus(lockStakingAmount)),
     units
@@ -60,56 +66,70 @@ export const UpdateReserve = () => {
   };
 
   return (
-    <ModalPadding>
-      <h2 className="title unbounded" style={{ margin: '0.5rem' }}>
-        {t('updateReserve')}
-      </h2>
-      {warnings.length ? (
-        <ModalWarnings>
-          {warnings.map((warning, index) => (
-            <React.Fragment key={`warning_${index}`}>{warning}</React.Fragment>
-          ))}
-        </ModalWarnings>
-      ) : null}
-      <ReserveWrapper>
-        <div>
-          <h4 className="current">
-            {planckToUnit(reserve, units).toString()}{' '}
-          </h4>
-          <div className="slider">
-            <Slider
-              max={1}
-              value={planckToUnit(reserve, units).toNumber()}
-              step={0.1}
-              onChange={(val) => {
-                if (typeof val === 'number') {
-                  setReserve(
-                    new BigNumber(unitToPlanck(val.toString(), units))
-                  );
-                }
+    <>
+      <Close />
+      <ModalPadding>
+        <h2 className="title unbounded" style={{ margin: '0.5rem' }}>
+          {t('updateReserve')}
+        </h2>
+        {warnings.length ? (
+          <ModalWarnings>
+            {warnings.map((warning, index) => (
+              <React.Fragment key={`warning_${index}`}>
+                {warning}
+              </React.Fragment>
+            ))}
+          </ModalWarnings>
+        ) : null}
+        <ActionItem
+          text={t('updateReserve')}
+          inlineButton={
+            <ButtonHelp onClick={() => openHelp('Reserve Balance')} />
+          }
+        />
+
+        <ReserveWrapper>
+          <div>
+            <h4 className="current">
+              {planckToUnit(reserve, units).toString()}{' '}
+            </h4>
+            <div className="slider">
+              <Slider
+                max={['polkadot', 'westend'].includes(network.name) ? 3 : 1}
+                value={planckToUnit(reserve, units).toNumber()}
+                step={0.1}
+                onChange={(val) => {
+                  if (typeof val === 'number') {
+                    setReserve(
+                      new BigNumber(unitToPlanck(val.toString(), units))
+                    );
+                  }
+                }}
+                {...sliderProps}
+              />
+            </div>
+          </div>
+          <div className="confirm">
+            <ButtonPrimaryInvert
+              text={t('confirm')}
+              onClick={() => {
+                setStatus(0);
+                localStorage.setItem(
+                  `${network.name}_${activeAccount}_reserve`,
+                  planckToUnit(reserve, units).toString()
+                );
               }}
-              {...sliderProps}
+              disabled={
+                !accountHasSigner(activeAccount) ||
+                planckToUnit(reserve, units).toString() ===
+                  localStorage.getItem(
+                    `${network.name}_${activeAccount}_reserve`
+                  )
+              }
             />
           </div>
-        </div>
-        <div className="confirm">
-          <ButtonPrimaryInvert
-            text={t('confirm')}
-            onClick={() => {
-              setStatus(0);
-              localStorage.setItem(
-                `${network.name}_${activeAccount}_reserve`,
-                planckToUnit(reserve, units).toString()
-              );
-            }}
-            disabled={
-              !accountHasSigner(activeAccount) ||
-              planckToUnit(reserve, units).toString() ===
-                localStorage.getItem(`${network.name}_${activeAccount}_reserve`)
-            }
-          />
-        </div>
-      </ReserveWrapper>
-    </ModalPadding>
+        </ReserveWrapper>
+      </ModalPadding>
+    </>
   );
 };
