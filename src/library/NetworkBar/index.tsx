916a7bdc6c67500baee78382ceba34a50ae8c8be
Ross Bulat <ross@parity.io>
2023-06-10 14:12:12 +0700

    add VITE_LEGAL_DISCLOSURES_URL

--- src/library/NetworkBar/index.tsx
+++ src/library/NetworkBar/index.tsx
@@ -1,6 +1,7 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { capitalizeFirstLetter } from '@polkadotcloud/utils';
 import { useApi } from 'contexts/Api';
 import { useOverlay } from 'contexts/Overlay';
 import { usePlugins } from 'contexts/Plugins';
@@ -8,7 +9,6 @@ import { useOutsideAlerter } from 'library/Hooks';
 import { usePrices } from 'library/Hooks/usePrices';
 import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { capitalizeFirstLetter } from 'Utils';
 import { Disclaimer } from './Disclaimer';
 import { Status } from './Status';
 import { NetworkInfo, Separator, Summary, Wrapper } from './Wrappers';
@@ -36,9 +36,10 @@ export const NetworkBar = () => {
   const animate = open ? 'maximised' : 'minimised';
   const ref = useRef(null);
 
-  const PRIVACY_URL = process.env.REACT_APP_PRIVACY_URL;
-  const DISCLAIMER_URL = process.env.REACT_APP_DISCLAIMER_URL;
-  const ORGANISATION = process.env.REACT_APP_ORGANISATION;
+  const PRIVACY_URL = import.meta.env.VITE_PRIVACY_URL;
+  const DISCLAIMER_URL = import.meta.env.VITE_DISCLAIMER_URL;
+  const ORGANISATION = import.meta.env.VITE_ORGANISATION;
+  const LEGAL_DISCLOSURES_URL = import.meta.env.VITE_LEGAL_DISCLOSURES_URL;
 
   const [networkName, setNetworkName] = useState<string>(
     capitalizeFirstLetter(network.name)
@@ -90,6 +91,21 @@ export const NetworkBar = () => {
               <p>
                 <a href={DISCLAIMER_URL} target="_blank" rel="noreferrer">
                   {t('disclaimer')}
+                </a>
+              </p>
+            </>
+          )}
+          <Separator />
+          {LEGAL_DISCLOSURES_URL !== undefined && (
+            <>
+              <Separator />
+              <p>
+                <a
+                  href={LEGAL_DISCLOSURES_URL}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {t('legalDisclosures')}
                 </a>
               </p>
             </>
