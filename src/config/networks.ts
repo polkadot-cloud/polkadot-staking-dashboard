322236d7c0c28baee5112bd92e16601326b77829
Ada <86626977+AdaJane@users.noreply.github.com>
2023-06-13 14:51:35 -0400

    Hide the "Mainnet" entry from the networks list to prevent tester (#12)
    confusion
    
    Co-authored-by: Ada <>

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -8,58 +8,58 @@ import { ReactComponent as CreditcoinLogoSVG } from 'img/logo_creditcoin.svg';
 import type { Networks } from 'types';
 
 export const NetworkList: Networks = {
-  creditcoin: {
-    name: 'creditcoin',
-    endpoints: {
-      rpc: 'wss://rpc.mainnet.creditcoin.network/ws',
-      lightClient: null,
-    },
-    namespace: '09573a3526818a8ecd6eb92f60f1175d',
-    api: {
-      unit: 'CTC',
-      priceTicker: 'CTCUSDT',
-    },
-    params: {
-      ...DefaultParams,
-    },
-    ss58: 42,
-    unit: 'CTC',
-    units: 18,
-    brand: {
-      icon: CreditcoinIconSVG,
-      logo: {
-        svg: CreditcoinLogoSVG,
-        width: '7.2em',
-      },
-      inline: {
-        svg: CreditcoinIconSVG,
-        size: '1.05em',
-      },
-    },
-    colors: {
-      primary: {
-        light: 'rgb(211, 48, 121)',
-        dark: 'rgb(211, 48, 121)',
-      },
-      secondary: {
-        light: '#552bbf',
-        dark: '#6d39ee',
-      },
-      stroke: {
-        light: 'rgb(211, 48, 121)',
-        dark: 'rgb(211, 48, 121)',
-      },
-      transparent: {
-        light: 'rgb(211, 48, 121, 0.05)',
-        dark: 'rgb(211, 48, 121, 0.05)',
-      },
-      pending: {
-        light: 'rgb(211, 48, 121, 0.33)',
-        dark: 'rgb(211, 48, 121, 0.33)',
-      },
-    },
-    subscanEndpoint: 'http://127.0.0.1:4399',
-  },
+  // creditcoin: {
+  //   name: 'creditcoin',
+  //   endpoints: {
+  //     rpc: 'wss://rpc.mainnet.creditcoin.network/ws',
+  //     lightClient: null,
+  //   },
+  //   namespace: '09573a3526818a8ecd6eb92f60f1175d',
+  //   api: {
+  //     unit: 'CTC',
+  //     priceTicker: 'CTCUSDT',
+  //   },
+  //   params: {
+  //     ...DefaultParams,
+  //   },
+  //   ss58: 42,
+  //   unit: 'CTC',
+  //   units: 18,
+  //   brand: {
+  //     icon: CreditcoinIconSVG,
+  //     logo: {
+  //       svg: CreditcoinLogoSVG,
+  //       width: '7.2em',
+  //     },
+  //     inline: {
+  //       svg: CreditcoinIconSVG,
+  //       size: '1.05em',
+  //     },
+  //   },
+  //   colors: {
+  //     primary: {
+  //       light: 'rgb(211, 48, 121)',
+  //       dark: 'rgb(211, 48, 121)',
+  //     },
+  //     secondary: {
+  //       light: '#552bbf',
+  //       dark: '#6d39ee',
+  //     },
+  //     stroke: {
+  //       light: 'rgb(211, 48, 121)',
+  //       dark: 'rgb(211, 48, 121)',
+  //     },
+  //     transparent: {
+  //       light: 'rgb(211, 48, 121, 0.05)',
+  //       dark: 'rgb(211, 48, 121, 0.05)',
+  //     },
+  //     pending: {
+  //       light: 'rgb(211, 48, 121, 0.33)',
+  //       dark: 'rgb(211, 48, 121, 0.33)',
+  //     },
+  //   },
+  //   subscanEndpoint: 'http://127.0.0.1:4399',
+  // },
   creditcoinTest: {
     name: 'creditcoinTest',
     endpoints: {
