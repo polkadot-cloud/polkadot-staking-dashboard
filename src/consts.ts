db0f3bc0cd3ffccaf685bc01b98c544642c79c06
Ada <unknown>
2023-06-14 09:37:26 -0400

    Set TestNet as default

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,7 @@ export const AppVersion = '1.0.8';
 export const BaseURL = '';
 export const DappName = 'Creditcoin Staking Dashboard';
 export const CreditcoinUrl = 'https://creditcoin.org';
-export const DefaultNetwork = 'creditcoin';
+export const DefaultNetwork = 'creditcoinTest';
 
 /*
  * Data Structure Helpers
