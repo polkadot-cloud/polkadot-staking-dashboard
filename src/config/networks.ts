bdf8f10ffebdc268519f4f3c79ef4286c9366d05
Ada Anderson <ada.anderson@gluwa.com>
2023-06-27 13:58:42 -0400

    Remove Devnet entry from Networks List

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -112,56 +112,56 @@ export const NetworkList: Networks = {
     },
     subscanEndpoint: 'https://subscan-testnet.creditcoin.network/',
   },
-  creditcoinDev: {
-    name: 'creditcoinDev',
-    endpoints: {
-      rpc: 'wss://rpc.devnet.creditcoin.network/ws',
-      lightClient: null,
-    },
-    namespace: '09573a3526818a8ecd6eb92f60f1175d',
-    api: {
-      unit: 'CTC',
-      priceTicker: 'CTCUSDT',
-    },
-    params: {
-      ...DefaultParams,
-    },
-    ss58: 42,
-    unit: 'CTC',
-    units: 18,
-    brand: {
-      icon: CreditcoinIconSVG,
-      logo: {
-        svg: CreditcoinLogoSVG,
-        width: '7.2em',
-      },
-      inline: {
-        svg: CreditcoinIconSVG,
-        size: '1.05em',
-      },
-    },
-    colors: {
-      primary: {
-        light: '#9fccaa',
-        dark: '#9fccaa',
-      },
-      secondary: {
-        light: '#9fccaa',
-        dark: '#9fccaa',
-      },
-      stroke: {
-        light: '#9fccaa',
-        dark: '#9fccaa',
-      },
-      transparent: {
-        light: 'rgb(211, 48, 121, 0.05)',
-        dark: 'rgb(211, 48, 121, 0.05)',
-      },
-      pending: {
-        light: '#9fccaa',
-        dark: '#9fccaa',
-      },
-    },
-    subscanEndpoint: 'http://127.0.0.1:4399',
-  },
+  // creditcoinDev: {
+  //   name: 'creditcoinDev',
+  //   endpoints: {
+  //     rpc: 'wss://rpc.devnet.creditcoin.network/ws',
+  //     lightClient: null,
+  //   },
+  //   namespace: '09573a3526818a8ecd6eb92f60f1175d',
+  //   api: {
+  //     unit: 'CTC',
+  //     priceTicker: 'CTCUSDT',
+  //   },
+  //   params: {
+  //     ...DefaultParams,
+  //   },
+  //   ss58: 42,
+  //   unit: 'CTC',
+  //   units: 18,
+  //   brand: {
+  //     icon: CreditcoinIconSVG,
+  //     logo: {
+  //       svg: CreditcoinLogoSVG,
+  //       width: '7.2em',
+  //     },
+  //     inline: {
+  //       svg: CreditcoinIconSVG,
+  //       size: '1.05em',
+  //     },
+  //   },
+  //   colors: {
+  //     primary: {
+  //       light: '#9fccaa',
+  //       dark: '#9fccaa',
+  //     },
+  //     secondary: {
+  //       light: '#9fccaa',
+  //       dark: '#9fccaa',
+  //     },
+  //     stroke: {
+  //       light: '#9fccaa',
+  //       dark: '#9fccaa',
+  //     },
+  //     transparent: {
+  //       light: 'rgb(211, 48, 121, 0.05)',
+  //       dark: 'rgb(211, 48, 121, 0.05)',
+  //     },
+  //     pending: {
+  //       light: '#9fccaa',
+  //       dark: '#9fccaa',
+  //     },
+  //   },
+  //   subscanEndpoint: 'http://127.0.0.1:4399',
+  // },
 };
