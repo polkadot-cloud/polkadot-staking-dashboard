1d12067f3b9ce08ceba84ebd10b2ab9883ac86d7
Ross Bulat <ross@parity.io>
2023-07-17 00:03:19 +0700

    rm eslint-disable

--- src/library/QRCode/qrcode.ts
+++ src/library/QRCode/qrcode.ts
@@ -8,7 +8,6 @@ const qrcode: typeof _qrcode = _qrcode;
 
 // HACK The default function take string -> number[], the Uint8array is compatible
 // with that signature and the use thereof
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
 (qrcode as any).stringToBytes = (data: Uint8Array): Uint8Array => data;
 
 export { qrcode };
