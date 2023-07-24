6a0ff0df428ed72220b685d18389f7331096990d
Ross Bulat <ross@parity.io>
2023-07-24 18:14:55 +0700

    simpllfy footer

--- src/pages/Overview/BalanceFooter.tsx
+++ src/pages/Overview/BalanceFooter.tsx
@@ -1,24 +1,21 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { ButtonPrimaryInvert, Separator } from '@polkadotcloud/core-ui';
-import { useTranslation } from 'react-i18next';
 import { MoreWrapper } from './Wrappers';
 
 export const BalanceFooter = () => {
-  const { t } = useTranslation('pages');
-
   return (
     <MoreWrapper>
       <Separator />
-      <h4>{t('overview.moreResources')}</h4>
+      <h4>Other Balances</h4>
       <section>
         <ButtonPrimaryInvert
-          iconLeft={faCoins}
+          iconLeft={faGlobe}
           lg
           disabled={false}
-          text="Other Balances"
+          text="Manage"
         />
       </section>
     </MoreWrapper>
