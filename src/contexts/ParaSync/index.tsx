7a556fa112795dbf14adb6139a131bd28b33f386
Ross Bulat <ross@parity.io>
2023-07-24 11:30:27 +0700

    reference para sync status

--- src/contexts/ParaSync/index.tsx
+++ src/contexts/ParaSync/index.tsx
@@ -1,7 +1,9 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import type { Sync } from 'types';
 import type { ParaSyncContextInterface } from './types';
 import { defaultParaSyncContext } from './defaults';
 
@@ -10,6 +12,32 @@ export const ParaSyncProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  // Reference whether the app has been synced.
+  const isSyncing = useRef<Sync>('unsynced');
+
+  // We need to connect to `interlay` parachain and check the user's balances.
+  const syncBalances = async () => {
+    isSyncing.current = 'syncing';
+
+    // Connect to interlay via new api instance.
+    const wsProvider = new WsProvider(
+      'wss://interlay.api.onfinality.io/public-ws'
+    );
+
+    // eslint-disable-next-line
+    const api = await ApiPromise.create({ provider: wsProvider });
+    // TODO: query `tokens.accounts` to get token balances on chain.
+
+    // Sync complete.
+    isSyncing.current = 'synced';
+  };
+
+  useEffect(() => {
+    if (isSyncing.current === 'unsynced') {
+      syncBalances();
+    }
+  }, [isSyncing.current]);
+
   return (
     <ParaSyncContext.Provider value={{}}>{children}</ParaSyncContext.Provider>
   );
