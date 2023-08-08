13aad67da12ef4c08eb6c3e268b728a3903d60b4
Ross Bulat <ross@parity.io>
2023-08-08 12:53:00 +0700

    test ci

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -62,7 +62,7 @@ export const Overview = () => {
 
   return (
     <>
-      <PageTitle title={t('overview.overview')} />
+      <PageTitle title={`✅ ${t('overview.overview')}`} />
       <PageRow>
         <PageHeading>
           <ActiveAccounts />
