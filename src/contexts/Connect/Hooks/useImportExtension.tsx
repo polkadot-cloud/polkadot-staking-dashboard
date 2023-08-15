7bb89f2e08b2da357116088722452f647721d5d9
Ross Bulat <ross@parity.io>
2023-08-15 11:59:15 +0700

    rm uneeded casting

--- src/contexts/Connect/Hooks/useImportExtension.tsx
+++ src/contexts/Connect/Hooks/useImportExtension.tsx
@@ -65,12 +65,10 @@ export const useImportExtension = () => {
     keyring.setSS58Format(network.ss58);
 
     // remove accounts that do not contain correctly formatted addresses.
-    newAccounts = newAccounts.filter((i: ExtensionAccount) =>
-      isValidAddress(i.address)
-    );
+    newAccounts = newAccounts.filter((i) => isValidAddress(i.address));
 
     // reformat addresses to ensure correct ss58 format
-    newAccounts.forEach(async (account: ExtensionAccount) => {
+    newAccounts.forEach(async (account) => {
       const { address } = keyring.addFromAddress(account.address);
       account.address = address;
       return account;
