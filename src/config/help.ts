a79811a426912670e4a1d531817c52fa98421358
rafael <rvalle@privaz.io>
2023-08-30 15:42:20 +0200

    activated help under validator key

--- src/config/help.ts
+++ src/config/help.ts
@@ -131,6 +131,7 @@ export const HelpConfig: HelpItems = [
       'Commission',
       'Over Subscribed',
       'Blocked Nominations',
+      'Rewards By Country And Network',
     ],
     external: [
       [
