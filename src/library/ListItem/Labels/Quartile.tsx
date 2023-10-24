b84dbd0c676359f28bf2147b016164ce25b23d6b
Ross Bulat <ross@parity.io>
2023-10-24 17:55:10 +0800

    chore: amend text

--- src/library/ListItem/Labels/Quartile.tsx
+++ src/library/ListItem/Labels/Quartile.tsx
@@ -7,15 +7,16 @@ import { useTooltip } from 'contexts/Tooltip';
 import { useTranslation } from 'react-i18next';
 
 export const Quartile = ({ address }: { address: string }) => {
-  const { t } = useTranslation('library');
+  const { t } = useTranslation();
   const { setTooltipTextAndOpen } = useTooltip();
   const { validatorEraPointsHistory, erasRewardPointsFetched } =
     useValidators();
 
   const quartile = validatorEraPointsHistory[address]?.quartile;
-  const tooltipText = `${MaxEraRewardPointsEras} ${t(
-    'dayPerformanceStanding'
-  )}`;
+  const tooltipText = `${t('dayPerformanceStanding', {
+    count: MaxEraRewardPointsEras,
+    ns: 'library',
+  })}`;
 
   if (erasRewardPointsFetched !== 'synced') return null;
 
@@ -26,7 +27,9 @@ export const Quartile = ({ address }: { address: string }) => {
       onMouseMove={() => setTooltipTextAndOpen(tooltipText)}
       style={{ cursor: 'default' }}
     >
-      {![100, undefined].includes(quartile) ? `${t('top')} ${quartile}%` : ``}
+      {![100, undefined].includes(quartile)
+        ? `${t('top', { ns: 'library' })} ${quartile}%`
+        : ``}
     </div>
   );
 };
