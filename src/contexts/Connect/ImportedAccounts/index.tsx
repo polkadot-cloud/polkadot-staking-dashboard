aa827ea43383f1d4a9e6f679d0b9a5e75360312b
Ross Bulat <ross@parity.io>
2023-12-19 12:43:39 +0700

    useCallback on ImportedAccounts context

--- src/contexts/Connect/ImportedAccounts/index.tsx
+++ src/contexts/Connect/ImportedAccounts/index.tsx
@@ -27,7 +27,7 @@ export const ImportedAccountsProvider = ({
 
   // Gets an account from `allAccounts`.
   //
-  // Caches the function when list of accounts update.
+  // Caches the function when imported accounts update.
   const getAccount = useCallback(
     (who: MaybeAddress) => {
       return allAccounts.find(({ address }) => address === who) || null;
@@ -35,29 +35,52 @@ export const ImportedAccountsProvider = ({
     [allAccounts]
   );
 
-  const isReadOnlyAccount = (address: MaybeAddress) => {
-    const account = getAccount(address) ?? {};
+  // Checks if an address is a read-only account.
+  //
+  // Caches the function when imported accounts update.
+  const isReadOnlyAccount = useCallback(
+    (address: MaybeAddress) => {
+      const account = getAccount(address) ?? {};
+      if (Object.prototype.hasOwnProperty.call(account, 'addedBy')) {
+        const { addedBy } = account as ExternalAccount;
+        return addedBy === 'user';
+      }
+      return false;
+    },
+    [allAccounts]
+  );
 
-    if (Object.prototype.hasOwnProperty.call(account, 'addedBy')) {
-      const { addedBy } = account as ExternalAccount;
-      return addedBy === 'user';
-    }
-    return false;
-  };
+  // Checks whether an account can sign transactions.
+  //
+  // Caches the function when imported accounts update.
+  const accountHasSigner = useCallback(
+    (address: MaybeAddress) => {
+      return (
+        allAccounts.find(
+          (account) =>
+            account.address === address && account.source !== 'external'
+        ) !== undefined
+      );
+    },
+    [allAccounts]
+  );
 
-  // Checks whether an account can sign transactions
-  const accountHasSigner = (address: MaybeAddress) =>
-    allAccounts.find(
-      (a) => a.address === address && a.source !== 'external'
-    ) !== undefined;
-
-  // Checks whether an account needs manual signing. This is the case for Ledger accounts,
-  // transactions of which cannot be automatically signed by a provided `signer` as is the case with
-  // extensions.
-  const requiresManualSign = (address: MaybeAddress) =>
-    allAccounts.find(
-      (a) => a.address === address && ManualSigners.includes(a.source)
-    ) !== undefined;
+  // Checks whether an account needs manual signing.
+  //
+  // This is the case for accounts imported from hardware wallets, transactions of which cannot be
+  // automatically signed by a provided `signer` as is the case with web extensions.
+  //
+  // Caches the function when imported accounts update.
+  const requiresManualSign = useCallback(
+    (address: MaybeAddress) => {
+      return (
+        allAccounts.find(
+          (a) => a.address === address && ManualSigners.includes(a.source)
+        ) !== undefined
+      );
+    },
+    [allAccounts]
+  );
 
   return (
     <ImportedAccountsContext.Provider
