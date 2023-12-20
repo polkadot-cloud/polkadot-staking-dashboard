c3ee0efe2bec486ba59455e2e517f0834338b0e7
Ross Bulat <ross@parity.io>
2023-12-20 17:35:10 +0700

    add console.log

--- src/Router.tsx
+++ src/Router.tsx
@@ -89,6 +89,8 @@ export const RouterInner = () => {
   // References to outer containers
   const mainInterfaceRef = useRef<HTMLDivElement>(null);
 
+  console.log('router re-rendered');
+
   return (
     <ErrorBoundary FallbackComponent={ErrorFallbackApp}>
       {/* Notification popups */}
