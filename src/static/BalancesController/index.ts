f0ee2f3a841e63376800119166c3d674cd5e51ce
Ross Bulat <ross@parity.io>
2024-01-12 11:11:38 +0700

    add subscriptions to BalancesController

--- src/static/BalancesController/index.ts
+++ src/static/BalancesController/index.ts
@@ -2,6 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import type { VoidFn } from '@polkadot-cloud/react/types';
+import { rmCommas } from '@polkadot-cloud/utils';
+import BigNumber from 'bignumber.js';
+import type { Balances, Ledger, UnlockChunkRaw } from 'contexts/Balances/types';
+import { APIController } from 'static/APController';
+import type { AnyApi } from 'types';
 
 export class BalancesController {
   // ------------------------------------------------------
@@ -10,6 +15,14 @@ export class BalancesController {
 
   // Accounts that are being subscribed to.
   static accounts: string[] = [];
+
+  // Account ledgers, populated by api callbacks.
+  // TODO: update `Ledger` type to omit `address`.
+  static ledgers: Record<string, Ledger> = {};
+
+  // Account balances, populated by api callbacks.
+  // TODO: update `Balances` type to omit `address`.
+  static balances: Record<string, Balances> = {};
 
   // Unsubscribe objects.
   static _unsubs: Record<string, VoidFn> = {};
@@ -20,7 +33,109 @@ export class BalancesController {
 
   // Subscribes new accounts and unsubscribes & removes removed accounts.
   static syncAccounts = async (newAccounts: string[]): Promise<void> => {
+    const { api } = APIController;
+
     console.log('syncing ', newAccounts.length, ' accounts');
+
+    // Handle accounts that have been removed.
+    this.handleRemovedAccounts(newAccounts);
+
+    // Determine new accounts that need to be subscribed to.
+    const accountsAdded = newAccounts.filter(
+      (account) => !this.accounts.includes(account)
+    );
+
+    // Subscribe to and add new accounts data.
+    accountsAdded.forEach(async (address) => {
+      this.accounts.push(address);
+
+      const unsub = await api.queryMulti<AnyApi>(
+        [
+          [api.query.staking.ledger, address],
+          [api.query.system.account, address],
+          [api.query.balances.locks, address],
+        ],
+        async ([ledgerResult, accountResult, locksResult]) => {
+          this.handleLedgerCallback(address, ledgerResult);
+          this.handleAccountCallback(address, accountResult, locksResult);
+        }
+      );
+      this._unsubs[address] = unsub;
+    });
+  };
+
+  // Remove accounts that no longer exist.
+  static handleRemovedAccounts = (newAccounts: string[]) => {
+    // Determine removed accounts.
+    const accountsRemoved = this.accounts.filter(
+      (account) => !newAccounts.includes(account)
+    );
+    // Unsubscribe from removed account subscriptions.
+    accountsRemoved.forEach((account) => {
+      this._unsubs[account]();
+      delete this._unsubs[account];
+    });
+    // Remove removed accounts from class.
+    this.accounts = this.accounts.filter(
+      (account) => !accountsRemoved.includes(account)
+    );
+  };
+
+  // Handle ledger callback.
+  static handleLedgerCallback = (address: string, result: AnyApi): void => {
+    const ledger = result.unwrapOr(null);
+
+    // If ledger is null, remove from class data and exit early.
+    if (ledger === null) {
+      delete this.ledgers[address];
+      return;
+    }
+
+    const { stash, total, active, unlocking } = ledger;
+
+    // Add stash as external account if not presently imported.
+    if (!this.accounts.includes(stash.toString())) {
+      // TODO: send event to `Balances` context and add external account.
+      // ----------------------------------------------------------------
+      // const result = addExternalAccount(stash.toString(), 'system');
+      // if (result) {
+      //   addOrReplaceOtherAccount(result.account, result.type);
+      // }
+      // ----------------------------------------------------------------
+    }
+
+    this.ledgers[address] = {
+      address,
+      stash: stash.toString(),
+      active: this.balanceToBigNumber(active.toString()),
+      total: this.balanceToBigNumber(total.toString()),
+      unlocking: unlocking.toHuman().map(({ era, value }: UnlockChunkRaw) => ({
+        era: Number(rmCommas(era)),
+        value: this.balanceToBigNumber(value).toString(),
+      })),
+    };
+  };
+
+  // Handle account callback.
+  static handleAccountCallback = (
+    address: string,
+    { data: accountData, nonce }: AnyApi,
+    locksResult: AnyApi
+  ): void => {
+    this.balances[address] = {
+      address,
+      nonce: nonce.toNumber(),
+      balance: {
+        free: this.balanceToBigNumber(accountData.free.toString()),
+        reserved: this.balanceToBigNumber(accountData.reserved.toString()),
+        frozen: this.balanceToBigNumber(accountData.frozen.toString()),
+      },
+      locks: locksResult.toHuman().map((lock: AnyApi) => ({
+        ...lock,
+        id: lock.id.trim(),
+        amount: this.balanceToBigNumber(lock.amount),
+      })),
+    };
   };
 
   // ------------------------------------------------------
@@ -35,4 +150,12 @@ export class BalancesController {
     this._unsubs = {};
     this.accounts = [];
   };
+
+  // ------------------------------------------------------
+  // Class helpers.
+  // ------------------------------------------------------
+
+  // Converts a balance string into a `BigNumber`.
+  static balanceToBigNumber = (value: string): BigNumber =>
+    new BigNumber(rmCommas(value));
 }
