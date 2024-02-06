5e14917fe4c484d1618eeafd17fec78669d09a2e
Ross Bulat <ross@parity.io>
2024-02-06 13:31:19 +0700

    SyncIDs to top

--- src/static/SyncController/types.ts
+++ src/static/SyncController/types.ts
@@ -1,16 +1,16 @@
 // Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-export interface SyncEvent {
-  id: SyncID;
-  status: SyncStatus;
-}
-
 export type SyncID =
   | 'initialization'
   | 'balances'
   | 'era-stakers'
   | 'active-pools';
+
+export interface SyncEvent {
+  id: SyncID;
+  status: SyncStatus;
+}
 
 export type SyncStatus = 'syncing' | 'complete';
 
