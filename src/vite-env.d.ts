c89fdf5e48c95f98a8a529ecadd9a1e9b953b378
Ross Bulat <ross@parity.io>
2024-02-18 13:38:21 +0700

    chore: fix license

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 // Copyright 2023 @paritytech/polkadot-live authors & contributors
-// SPDX-License-Identifier: GPL-3.0-only/// <reference types="vite/client" />
+// SPDX-License-Identifier: GPL-3.0-only
 
+/// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
 declare namespace JSX {
