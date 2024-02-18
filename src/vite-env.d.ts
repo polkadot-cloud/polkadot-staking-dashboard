6d72db6e0c5235a4eca07c4a646860d9b25eb558
Ross Bulat <ross@parity.io>
2024-02-18 13:35:08 +0700

    chore: check license

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
-/// <reference types="vite/client" />
+// Copyright 2023 @paritytech/polkadot-live authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only/// <reference types="vite/client" />
+
 /// <reference types="vite-plugin-svgr/client" />
 
 declare namespace JSX {
