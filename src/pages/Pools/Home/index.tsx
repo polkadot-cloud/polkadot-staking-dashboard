7ae53546dc18307460b6fad7f949204e07de109b
Ross Bulat <ross@parity.io>
2024-02-18 13:24:31 +0700

    fix license year

--- src/pages/Pools/Home/index.tsx
+++ src/pages/Pools/Home/index.tsx
@@ -1,4 +1,4 @@
-// Copyright 2023 @paritytech/polkadot-staking-dashboard authors & contributors
+// Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { PageRow, PageTitle, RowSection } from '@polkadot-cloud/react';
