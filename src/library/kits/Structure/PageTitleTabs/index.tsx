7d699b1ed42a5867c20af5999dd7eabfc9c8a719
Ross Bulat <ross@parity.io>
2024-02-18 16:22:03 +0700

    chore: fix license

--- src/library/kits/Structure/PageTitleTabs/index.tsx
+++ src/library/kits/Structure/PageTitleTabs/index.tsx
@@ -1,4 +1,4 @@
-/* @license Copyright 2024 @polkadot-cloud/library authors & contributors
+/* // Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
 import '@polkadot-cloud/core/css/base/structure/PageTitleTabs/index.css';
