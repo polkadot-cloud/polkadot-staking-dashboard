5c8da420b74a47ebdf2de1f61757828e35234fa5
Ross Bulat <ross@parity.io>
2024-02-19 13:19:07 +0700

    add StatBoxRow

---
+++ src/kits/Structure/StatBoxRow/index.tsx
@@ -0,0 +1,15 @@
+/* @license Copyright 2024 @polkadot-cloud/library authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
+
+import type { ComponentBase } from 'types';
+import { Wrapper } from './Wrapper';
+
+/**
+ * @name StatBoxRow
+ * @summary Used to house a row of `StatBox` items.
+ */
+export const StatBoxRow = ({ children, style }: ComponentBase) => (
+  <Wrapper className="page-padding" style={style}>
+    {children}
+  </Wrapper>
+);
