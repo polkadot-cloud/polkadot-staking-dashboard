e1b776a1422e688c1a5bdb71bc7e57140cb0a44f
Ross Bulat <ross@parity.io>
2024-02-19 13:28:18 +0700

    add Separator

--- src/pages/Nominate/Active/Status/index.tsx
+++ src/pages/Nominate/Active/Status/index.tsx
@@ -1,11 +1,11 @@
 // Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { Separator } from '@polkadot-cloud/react';
 import { CardWrapper } from 'library/Card/Wrappers';
 import { UnclaimedPayoutsStatus } from './UnclaimedPayoutsStatus';
 import { NominationStatus } from './NominationStatus';
 import { PayoutDestinationStatus } from './PayoutDestinationStatus';
+import { Separator } from 'kits/Structure/Separator';
 
 export const Status = ({ height }: { height: number }) => (
   <CardWrapper height={height}>
