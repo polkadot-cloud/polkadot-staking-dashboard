47ec1d5c7339a77608967a3b7f9bcb1a0a48efc7
Ross Bulat <ross@parity.io>
2024-02-26 14:02:39 +0700

    use local ValidatorCommunity

--- src/contexts/Community/index.tsx
+++ src/contexts/Community/index.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import type { ValidatorEntry } from '@polkadot-cloud/assets/types';
-import { ValidatorCommunity } from '@polkadot-cloud/assets/validators';
+import { ValidatorCommunity } from 'config/validators';
 import { shuffle } from '@polkadot-cloud/utils';
 import type { ReactNode } from 'react';
 import { createContext, useContext, useState } from 'react';
