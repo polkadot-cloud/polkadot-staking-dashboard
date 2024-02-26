058cb24dd52e41ebc254522e162c9ba55cb74b5b
Ross Bulat <ross@parity.io>
2024-02-26 14:20:42 +0700

    feat(refactor): stricter validator operator svgs (#1986)

--- src/config/validators/P2POrg.tsx
+++ src/config/validators/P2POrg.tsx
@@ -1,11 +1,5 @@
 const P2POrg = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="744"
-    height="744"
-    fill="none"
-    viewBox="0 0 744 744"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 744 744">
     <path fill="#136BFF" d="M0 0H744V744H0z"></path>
     <path
       fill="#fff"
