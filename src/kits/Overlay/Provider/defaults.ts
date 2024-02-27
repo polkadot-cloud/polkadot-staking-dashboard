9aaa7ffe7d13a73c3d068901f1599962f1218904
Ross Bulat <ross@parity.io>
2024-02-27 13:04:52 +0700

    replace more licenses

--- src/kits/Overlay/Provider/defaults.ts
+++ src/kits/Overlay/Provider/defaults.ts
@@ -1,5 +1,5 @@
-/* @license Copyright 2024 @polkadot-cloud/library authors & contributors",
-"SPDX-License-Identifier: GPL-3.0-only */
+// Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function, no-unused-vars */
 
 import { unimplemented } from '@w3ux/utils';
