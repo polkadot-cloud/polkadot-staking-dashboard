d2d52ed5b227c69efcb5e98b8dd8f0608acc62f4
Ross Bulat <ross@parity.io>
2024-03-09 20:43:39 +0700

    unneeded ?.

--- src/static/BalancesController/index.ts
+++ src/static/BalancesController/index.ts
@@ -129,7 +129,7 @@ export class BalancesController {
     );
     // Unsubscribe from removed account subscriptions.
     accountsRemoved.forEach((account) => {
-      if (this._unsubs?.[account]) {
+      if (this._unsubs[account]) {
         this._unsubs[account]();
       }
       delete this._unsubs[account];
