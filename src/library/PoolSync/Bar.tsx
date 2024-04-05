dec871912996ce1c90d7976cc680ac1e78760722
Ross Bulat <ross@parity.io>
2024-04-05 20:51:53 +0700

    progress bar

---
+++ src/library/PoolSync/Bar.tsx
@@ -0,0 +1,31 @@
+// Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import BigNumber from 'bignumber.js';
+import { usePoolPerformance } from 'contexts/Pools/PoolPerformance';
+
+export const PoolSyncBar = () => {
+  const { getPoolPerformanceTask } = usePoolPerformance();
+
+  // Get the pool performance task to determine if performance data is ready.
+  const poolJoinPerformanceTask = getPoolPerformanceTask('pool_join');
+
+  // Calculate syncing status.
+  const { startEra, currentEra, endEra } = poolJoinPerformanceTask;
+  const totalEras = startEra.minus(endEra);
+  const erasPassed = startEra.minus(currentEra);
+  const percentPassed = erasPassed.isEqualTo(0)
+    ? new BigNumber(0)
+    : erasPassed.dividedBy(totalEras).multipliedBy(100);
+
+  return (
+    <div className="loader">
+      <div>
+        <span
+          className="progress"
+          style={{ width: `${percentPassed}%` }}
+        ></span>
+      </div>
+    </div>
+  );
+};
