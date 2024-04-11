459da2477b5f1e032cf974ccb819be85a7a682be
frankli-dev <51565705+frankli-dev@users.noreply.github.com>
2024-04-11 07:59:30 -0600

    fix: Show border bottom only if dashboard tips is shown (#2076)

--- src/pages/Overview/StakeStatus/Wrappers.ts
+++ src/pages/Overview/StakeStatus/Wrappers.ts
@@ -4,7 +4,7 @@
 import styled from 'styled-components';
 import { PageWidthMediumThreshold } from 'consts';
 
-export const StatusWrapper = styled.div`
+export const StatusWrapper = styled.div<{ borderBottom: boolean }>`
   display: flex;
   flex-wrap: wrap;
   padding: 1.5rem 1.5rem 0 1.5rem;
@@ -30,7 +30,8 @@ export const StatusWrapper = styled.div`
     }
 
     > section {
-      border-bottom: 1px solid var(--border-primary-color);
+      border-bottom: ${(props) =>
+        props.borderBottom ? '1px solid var(--border-primary-color)' : 'none'};
       padding-bottom: 0.75rem;
       @media (max-width: ${PageWidthMediumThreshold}px) {
         padding-bottom: 0.5rem;
