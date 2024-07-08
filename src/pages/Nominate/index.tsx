a5c63358e86b18bae92064d579ce4d28efd96481
Ross Bulat <ross@jkrb.io>
2024-07-08 09:07:20 +0100

    hide tabs if polkawatch disabled

--- src/pages/Nominate/index.tsx
+++ src/pages/Nominate/index.tsx
@@ -7,24 +7,38 @@ import { Wrapper } from './Wrappers';
 import { PageTitle } from '../../kits/Structure/PageTitle';
 import { useTranslation } from 'react-i18next';
 import type { PageTitleTabProps } from 'kits/Structure/PageTitleTabs/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePlugins } from 'contexts/Plugins';
 
 export const Nominate = () => {
   const { t } = useTranslation();
+  const { pluginEnabled } = usePlugins();
+
   const [activeTab, setActiveTab] = useState<number>(0);
 
-  const tabs: PageTitleTabProps[] = [
-    {
-      title: t('overview', { ns: 'base' }),
-      active: activeTab === 0,
-      onClick: () => setActiveTab(0),
-    },
-    {
-      title: t('decentralization', { ns: 'base' }),
-      active: activeTab === 1,
-      onClick: () => setActiveTab(1),
-    },
-  ];
+  // Only include tabs if the Polkawatch plugin is enabled.
+  let tabs: PageTitleTabProps[] | undefined = undefined;
+  if (pluginEnabled('polkawatch')) {
+    tabs = [
+      {
+        title: t('overview', { ns: 'base' }),
+        active: activeTab === 0,
+        onClick: () => setActiveTab(0),
+      },
+      {
+        title: t('decentralization', { ns: 'base' }),
+        active: activeTab === 1,
+        onClick: () => setActiveTab(1),
+      },
+    ];
+  }
+
+  // Go back to the first tab if the Polkawatch plugin is disabled.
+  useEffect(() => {
+    if (!pluginEnabled('polkawatch')) {
+      setActiveTab(0);
+    }
+  }, [pluginEnabled('polkawatch')]);
 
   return (
     <Wrapper>
