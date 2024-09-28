725c7e0cddd378a78af95cbe4ced632aff6b3542
Ross Bulat <ross@jkrb.io>
2024-09-28 17:25:50 +0700

    + browse pools button

--- src/pages/Pools/Home/Status/NewMember.tsx
+++ src/pages/Pools/Home/Status/NewMember.tsx
@@ -13,10 +13,12 @@ import { usePoolPerformance } from 'contexts/Pools/PoolPerformance';
 import { PoolSync } from 'library/PoolSync';
 import { useJoinPools } from 'contexts/Pools/JoinPools';
 import { StyledLoader } from 'library/PoolSync/Loader';
+import { usePoolsTabs } from '../context';
 
 export const NewMember = ({ syncing }: NewMemberProps) => {
   const { t } = useTranslation();
   const { poolsForJoin } = useJoinPools();
+  const { setActiveTab } = usePoolsTabs();
   const { openCanvas } = useOverlay().canvas;
   const { startJoinPoolFetch } = useJoinPools();
   const { getPoolPerformanceTask } = usePoolPerformance();
@@ -86,7 +88,7 @@ export const NewMember = ({ syncing }: NewMemberProps) => {
             <section>
               <div className="buttons">
                 <div
-                  className={`button secondary standalone${createDisabled ? ` disabled` : ``}`}
+                  className={`button standalone secondary ${createDisabled ? ` disabled` : ``}`}
                 >
                   <button
                     onClick={() =>
@@ -99,6 +101,11 @@ export const NewMember = ({ syncing }: NewMemberProps) => {
                     disabled={createDisabled}
                   >
                     {t('pools.createPool', { ns: 'pages' })}
+                  </button>
+                </div>
+                <div className={`button standalone secondary`}>
+                  <button onClick={() => setActiveTab(1)}>
+                    {t('pools.browsePools', { ns: 'pages' })}
                   </button>
                 </div>
               </div>
