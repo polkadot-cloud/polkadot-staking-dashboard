c26ca0590784326b66f5110e683d839d28edd1c5
Ross Bulat <ross@jkrb.io>
2024-10-31 16:03:42 +0700

    update to `stringToBn`

--- src/model/Query/StakingConstants/index.ts
+++ src/model/Query/StakingConstants/index.ts
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import type { ApiPromise } from '@polkadot/api';
-import { stringToBigNumber } from '@w3ux/utils';
 import BigNumber from 'bignumber.js';
 import { NetworkList } from 'config/networks';
+import { stringToBn } from 'Utils';
 
 export class StakingConstants {
   // ------------------------------------------------------
@@ -42,35 +42,35 @@ export class StakingConstants {
 
     return {
       bondDuration: consts[0]
-        ? stringToBigNumber(consts[0].toString())
+        ? stringToBn(consts[0].toString())
         : this.FALLBACK.BONDING_DURATION,
       maxNominations: consts[1]
-        ? stringToBigNumber(consts[1].toString())
+        ? stringToBn(consts[1].toString())
         : this.FALLBACK.MAX_NOMINATIONS,
       sessionsPerEra: consts[2]
-        ? stringToBigNumber(consts[2].toString())
+        ? stringToBn(consts[2].toString())
         : this.FALLBACK.SESSIONS_PER_ERA,
       maxElectingVoters: consts[3]
-        ? stringToBigNumber(consts[3].toString())
+        ? stringToBn(consts[3].toString())
         : this.FALLBACK.MAX_ELECTING_VOTERS,
       expectedBlockTime: consts[4]
-        ? stringToBigNumber(consts[4].toString())
+        ? stringToBn(consts[4].toString())
         : this.FALLBACK.EXPECTED_BLOCK_TIME,
       epochDuration: consts[5]
-        ? stringToBigNumber(consts[5].toString())
+        ? stringToBn(consts[5].toString())
         : this.FALLBACK.EPOCH_DURATION,
       existentialDeposit: consts[6]
-        ? stringToBigNumber(consts[6].toString())
+        ? stringToBn(consts[6].toString())
         : new BigNumber(0),
       historyDepth: consts[7]
-        ? stringToBigNumber(consts[7].toString())
+        ? stringToBn(consts[7].toString())
         : new BigNumber(0),
       fastUnstakeDeposit: consts[8]
-        ? stringToBigNumber(consts[8].toString())
+        ? stringToBn(consts[8].toString())
         : new BigNumber(0),
       poolsPalletId: consts[9] ? consts[9].toU8a() : new Uint8Array(0),
       maxExposurePageSize: consts[10]
-        ? stringToBigNumber(consts[10].toString())
+        ? stringToBn(consts[10].toString())
         : NetworkList[network].maxExposurePageSize,
     };
   }
