b8d72b56335b7f4eee966dc5d67dbb8473fe461c
Ross Bulat <ross@jkrb.io>
2024-11-14 13:21:02 +0700

    init `plugin-staking-api`

---
+++ packages/plugin-staking-api/src/index.tsx
@@ -0,0 +1,7 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { ApolloProvider } from '@apollo/client';
+
+export * from './Client';
+export { ApolloProvider };
