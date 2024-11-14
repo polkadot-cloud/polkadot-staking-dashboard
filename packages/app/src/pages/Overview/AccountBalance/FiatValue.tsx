6fd234d9a8e39138fa0bde29863e0e665472c146
Ross Bulat <ross@jkrb.io>
2024-11-14 13:52:36 +0700

    useTokenPrice for balance value

---
+++ packages/app/src/pages/Overview/AccountBalance/FiatValue.tsx
@@ -0,0 +1,40 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import BigNumber from 'bignumber.js';
+import { useNetwork } from 'contexts/Network';
+import { ApolloProvider, client, useTokenPrice } from 'plugin-staking-api';
+
+interface FiatValueProps {
+  totalBalance: BigNumber;
+}
+
+export const FiatValueInner = ({ totalBalance }: FiatValueProps) => {
+  const {
+    networkData: {
+      api: { unit },
+    },
+  } = useNetwork();
+  const { loading, error, data } = useTokenPrice({ ticker: `${unit}USDT` });
+
+  const price = loading || error ? 0 : data?.tokenPrice?.price || 0;
+
+  // Convert balance to fiat value.
+  const freeFiat = totalBalance.multipliedBy(
+    new BigNumber(price).decimalPlaces(2)
+  );
+
+  // Formatter for price feed.
+  const usdFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  return <>{usdFormatter.format(freeFiat.toNumber())}</>;
+};
+
+export const FiatValue = (props: FiatValueProps) => (
+  <ApolloProvider client={client}>
+    <FiatValueInner {...props} />
+  </ApolloProvider>
+);
