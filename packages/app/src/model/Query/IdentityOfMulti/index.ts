6cb0cd8799d1c127d7162b72fbdb4ab76df8f2bc
Ross Bulat <ross@jkrb.io>
2024-11-18 16:43:39 +0700

    add `IdentityOfMulti` class

---
+++ packages/app/src/model/Query/IdentityOfMulti/index.ts
@@ -0,0 +1,29 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { PapiApi } from 'model/Api/types';
+
+// TODO: Hook up and test.
+export class IdentityOfMulti {
+  #pApi: PapiApi;
+
+  #addresses: [string][];
+
+  constructor(pApi: PapiApi, addresses: [string][]) {
+    this.#pApi = pApi;
+    this.#addresses = addresses;
+  }
+
+  async fetch() {
+    try {
+      const result = await this.#pApi.query.Identity.IdentityOf.getValues(
+        this.#addresses
+      );
+      return result;
+    } catch (e) {
+      // Silently fail.
+    }
+
+    return null;
+  }
+}
