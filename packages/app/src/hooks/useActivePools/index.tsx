4b823b9108b7502f0a40ed4c08da3197dd4f343c
Ross Bulat <ross@jkrb.io>
2024-12-19 10:28:01 +0700

    feat(fix): Remove left pool from state (#2373)

--- packages/app/src/hooks/useActivePools/index.tsx
+++ packages/app/src/hooks/useActivePools/index.tsx
@@ -32,17 +32,18 @@ export const useActivePools = ({ onCallback, who }: ActivePoolsProps) => {
   // Handle report of new active pool data.
   const newActivePoolCallback = async (e: Event) => {
     if (isCustomEvent(e) && ActivePools.isValidNewActivePool(e)) {
-      const { address, pool, nominations } = e.detail
-      const { id } = pool
+      const { address, activePool, nominations } = e.detail
+      const { id } = activePool
 
       // Persist to active pools state for the specified account.
       if (address === who) {
         const newActivePools = { ...activePoolsRef.current }
-        newActivePools[id] = pool
+        newActivePools[id] = activePool
         setStateWithRef(newActivePools, setActivePools, activePoolsRef)
 
         const newPoolNominations = { ...poolNominationsRef.current }
         newPoolNominations[id] = nominations
+
         setStateWithRef(
           newPoolNominations,
           setPoolNominations,
@@ -54,6 +55,25 @@ export const useActivePools = ({ onCallback, who }: ActivePoolsProps) => {
       if (typeof onCallback === 'function') {
         await onCallback(e.detail)
       }
+    }
+  }
+
+  // Handle report of removed active pool
+  const removedActivePoolCallback = (e: Event) => {
+    if (isCustomEvent(e) && ActivePools.isValidRemovedActivePool(e)) {
+      const { poolId } = e.detail
+      const newActivePools = { ...activePoolsRef.current }
+      delete newActivePools[poolId]
+
+      const newPoolNominations = { ...poolNominationsRef.current }
+      delete newPoolNominations[poolId]
+
+      setStateWithRef(newActivePools, setActivePools, activePoolsRef)
+      setStateWithRef(
+        newPoolNominations,
+        setPoolNominations,
+        poolNominationsRef
+      )
     }
   }
 
@@ -88,6 +108,11 @@ export const useActivePools = ({ onCallback, who }: ActivePoolsProps) => {
   // Listen for new active pool events.
   const documentRef = useRef<Document>(document)
   useEventListener('new-active-pool', newActivePoolCallback, documentRef)
+  useEventListener(
+    'removed-active-pool',
+    removedActivePoolCallback,
+    documentRef
+  )
 
   return {
     activePools,
