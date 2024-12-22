672714a317673e982d152c345fc357d9d4992a7b
Ross Bulat <ross@jkrb.io>
2024-12-22 13:13:12 +0700

    add license

--- packages/plugin-staking-api/src/util.ts
+++ packages/plugin-staking-api/src/util.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { NominatorReward, PoolReward } from './types'
 
 export const isPoolReward = (
