e163a0fa411764e335e9e5a858f8ba1bc2140f62
Ross Bulat <ross@jkrb.io>
2025-01-02 10:51:52 +0700

    feat: normalise title w. polkadot

--- packages/app/src/library/PageWithTitle/index.tsx
+++ packages/app/src/library/PageWithTitle/index.tsx
@@ -2,21 +2,18 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import type { PageItem } from 'common-types'
-import { useNetwork } from 'contexts/Network'
 import { Helmet } from 'react-helmet'
 import { useTranslation } from 'react-i18next'
 import { Page } from 'ui-structure'
 
 export const PageWithTitle = ({ page }: { page: PageItem }) => {
   const { t } = useTranslation()
-  const { network } = useNetwork()
   const { Entry, key } = page
 
   return (
     <Page>
       <Helmet>
         <title>{`${t('title', {
-          context: `${network}`,
           ns: 'base',
         })} : ${t(key, { ns: 'base' })}`}</title>
       </Helmet>
