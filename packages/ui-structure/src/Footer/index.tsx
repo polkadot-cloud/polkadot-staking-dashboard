99c8e97e259001cce66b0694f3706e139c04b366
Ross Bulat <ross@jkrb.io>
2025-01-02 11:35:28 +0700

    revise footer

---
+++ packages/ui-structure/src/Footer/index.tsx
@@ -0,0 +1,19 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { ComponentBase } from '@w3ux/types'
+import { MaxPageWidth } from 'consts'
+import classes from './index.module.scss'
+
+/**
+ * @name Footer
+ * @summary Footer container.
+ */
+export const Footer = ({ children, style }: ComponentBase) => (
+  <div
+    className={classes.footer}
+    style={{ ...style, maxWidth: `${MaxPageWidth}px` }}
+  >
+    {children}
+  </div>
+)
