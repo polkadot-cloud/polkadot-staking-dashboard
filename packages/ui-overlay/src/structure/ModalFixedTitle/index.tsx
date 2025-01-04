df8ca1b62904001b2c0758dceaa0f209504f7e16
Ross Bulat <ross@jkrb.io>
2025-01-04 10:34:11 +0700

    refactor ModalFixedTitle

--- packages/ui-overlay/src/structure/ModalFixedTitle/index.tsx
+++ packages/ui-overlay/src/structure/ModalFixedTitle/index.tsx
@@ -1,27 +1,25 @@
 // Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { appendOrEmpty } from '@w3ux/utils'
+import classNames from 'classnames'
 import type { ForwardedRef } from 'react'
 import { forwardRef } from 'react'
-import type { ModalFixedTitleProps } from '../types'
+import type { ModalFixedTitleProps } from '../../types'
+import classes from './index.module.scss'
 
-/**
- * @name ModalFixedTitle
- * @summary Fixed the title.
- */
 export const ModalFixedTitle = forwardRef(
   (
     { children, style, withStyle }: ModalFixedTitleProps,
     ref?: ForwardedRef<HTMLDivElement>
-  ) => (
-    <div
-      ref={ref}
-      className={`modal-fixed-title${appendOrEmpty(withStyle, 'with-style')}`}
-      style={style}
-    >
-      {children}
-    </div>
-  )
+  ) => {
+    const allClasses = classNames(classes.fixedTitle, {
+      [classes.withStyle]: withStyle,
+    })
+    return (
+      <div ref={ref} className={allClasses} style={style}>
+        {children}
+      </div>
+    )
+  }
 )
 ModalFixedTitle.displayName = 'ModalFixedTitle'
