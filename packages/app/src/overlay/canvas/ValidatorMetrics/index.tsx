a0dc14644515895561c0a03b4ec28af31163cd3b
Ross Bulat <ross@jkrb.io>
2025-01-08 20:50:11 +0700

    some canvas boilerplate

--- packages/app/src/overlay/canvas/ValidatorMetrics/index.tsx
+++ packages/app/src/overlay/canvas/ValidatorMetrics/index.tsx
@@ -1,6 +1,35 @@
 // Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { Main } from 'ui-core/canvas'
+import { Polkicon } from '@w3ux/react-polkicon'
+import { AccountTitle, Main } from 'ui-core/canvas'
+import { useOverlay } from 'ui-overlay'
 
-export const ValidatorMetrics = () => <Main></Main>
+export const ValidatorMetrics = () => {
+  const {
+    config: { options },
+  } = useOverlay().canvas
+
+  const validator = options!.validator
+
+  return (
+    <Main>
+      <AccountTitle>
+        <div>
+          <div>
+            <Polkicon
+              address={validator}
+              background="transparent"
+              fontSize="4rem"
+            />
+          </div>
+          <div>
+            <div className="title">
+              <h1>Title</h1>
+            </div>
+          </div>
+        </div>
+      </AccountTitle>
+    </Main>
+  )
+}
