c6219454ccd2bb44dd37ec70e98be9c059613349
Ross Bulat <ross@jkrb.io>
2025-01-21 14:24:22 +0700

    `Spinner` to `ui-core/base`

---
+++ packages/ui-core/src/base/Spinner/index.tsx
@@ -0,0 +1,6 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import classes from './index.module.scss'
+
+export const Spinner = () => <div className={classes.spinner} />
