24f866371a10da757568c86222e1ea88a1837418
Ross Bulat <ross@jkrb.io>
2025-01-22 10:40:01 +0700

    add `toggleFeatures`

--- packages/app/src/config/plugins.ts
+++ packages/app/src/config/plugins.ts
@@ -4,6 +4,10 @@
 // List of available plugins.
 export type Plugin = 'staking_api' | 'subscan' | 'tips' | 'polkawatch'
 
+// Force plugins to always be enabled in production environment. NOTE: If you are forking the
+// staking dashboard and do not wish to use a plugin, you can remove it from this list.
+export const CompulsoryPluginsProd: Plugin[] = ['staking_api']
+
 export const PluginsList: Plugin[] = [
   'staking_api',
   'subscan',
--- packages/app/src/overlay/modals/Settings/index.tsx
+++ packages/app/src/overlay/modals/Settings/index.tsx
@@ -1,6 +1,7 @@
 // Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { CompulsoryPluginsProd, PluginsList } from 'config/plugins'
 import { usePlugins } from 'contexts/Plugins'
 import { Title } from 'library/Modal/Title'
 import { StatusButton } from 'library/StatusButton'
@@ -10,37 +11,30 @@ import { ContentWrapper } from '../Networks/Wrapper'
 
 export const Settings = () => {
   const { plugins, togglePlugin } = usePlugins()
-  const { t } = useTranslation('modals')
+  const { t } = useTranslation()
 
   return (
     <>
-      <Title title={t('settings')} />
+      <Title title={t('settings', { ns: 'modals' })} />
       <Padding>
         <ContentWrapper>
-          <h4>{t('togglePlugins')}</h4>
-          <StatusButton
-            checked={plugins.includes('staking_api')}
-            label="Staking API"
-            onClick={() => togglePlugin('staking_api')}
-          />
-          <StatusButton
-            checked={plugins.includes('subscan')}
-            label="Subscan API"
-            onClick={() => togglePlugin('subscan')}
-          />
-          <StatusButton
-            checked={plugins.includes('polkawatch')}
-            label="Polkawatch API"
-            onClick={() => togglePlugin('polkawatch')}
-          />
-
-          <h4>{t('toggleFeatures')}</h4>
-
-          <StatusButton
-            checked={plugins.includes('tips')}
-            label={t('dashboardTips')}
-            onClick={() => togglePlugin('tips')}
-          />
+          <h4>{t('togglePlugins', { ns: 'modals' })}</h4>
+          {PluginsList.map((plugin) => {
+            if (
+              import.meta.env.NODE_ENV === 'production' &&
+              CompulsoryPluginsProd.includes(plugin)
+            ) {
+              return null
+            }
+            return (
+              <StatusButton
+                key={plugin}
+                checked={plugins.includes(plugin)}
+                label={t(`plugin.${plugin}`, { ns: 'base' })}
+                onClick={() => togglePlugin(plugin)}
+              />
+            )
+          })}
         </ContentWrapper>
       </Padding>
     </>
