ba031f892041ca5ecf6ea3d69ebb6360c5f2c266
Ross Bulat <ross@jkrb.io>
2025-01-24 15:18:21 +0700

    add `validatorEraPointsBatch` query

---
+++ packages/plugin-staking-api/src/queries/validatorEraPointsBatch.tsx
@@ -0,0 +1,45 @@
+// Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { gql, useQuery } from '@apollo/client'
+import type { ValidatorEraPointsBatchResult } from '../types'
+
+const QUERY = gql`
+  query ValidatorEraPointsBatch(
+    $chain: String!
+    $validators: [String!]!
+    $fromEra: Int!
+    $depth: Int
+  ) {
+    validatorEraPointsBatch(
+      chain: $chain
+      validators: $validators
+      fromEra: $fromEra
+      depth: $depth
+    ) {
+      validator
+      points {
+        era
+        points
+        start
+      }
+    }
+  }
+`
+
+export const useValidatorEraPointsBatch = ({
+  chain,
+  validators,
+  fromEra,
+  depth,
+}: {
+  chain: string
+  validators: string[]
+  fromEra: number
+  depth?: number
+}): ValidatorEraPointsBatchResult => {
+  const { loading, error, data, refetch } = useQuery(QUERY, {
+    variables: { chain, validators, fromEra, depth },
+  })
+  return { loading, error, data, refetch }
+}
