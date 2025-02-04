6b1627d899ee13cd5515281af4ebd808f126bb9c
Joel Kenny <36333783+joeljkrb@users.noreply.github.com>
2025-02-03 19:38:53 -0500

    chore: fixes

--- packages/app/src/pages/Rewards/PayoutList/context.tsx
+++ packages/app/src/pages/Rewards/PayoutList/context.tsx
@@ -1,12 +1,11 @@
 // Copyright 2024 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { ListFormat } from 'library/PoolList/types'
-import type { PayoutListContextInterface } from 'pages/Pools/types'
+import type { ListFormat, PoolListContextProps } from 'library/PoolList/types'
 import type { ReactNode } from 'react'
 import { createContext, useContext, useState } from 'react'
 
-export const PayoutListContext = createContext<PayoutListContextInterface>({
+export const PayoutListContext = createContext<PoolListContextProps>({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setListFormat: (v: ListFormat) => {},
   listFormat: 'col',
