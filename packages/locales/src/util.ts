f526f8ef060774810972b9596138d007633779a3
Joel Kenny <36333783+joeljkrb@users.noreply.github.com>
2025-02-05 09:59:40 -0500

    feat: Implement browser language detection using navigator.language (#2494)
    Co-authored-by: Ross Bulat <ross@jkrb.io>

--- packages/locales/src/util.ts
+++ packages/locales/src/util.ts
@@ -20,10 +20,19 @@ export const getInitialLanguage = () => {
     return urlLng
   }
 
-  // fall back to localStorage if present.
+  // fall back to localStorage if present
   const localLng = localStorage.getItem('lng')
   if (localLng && Object.keys(locales).find((key) => key === localLng)) {
     return localLng
+  }
+
+  // fall back to browser language
+  const supportedBrowser = Object.entries(locales).find(([, { tag }]) =>
+    navigator.language.startsWith(tag)
+  )?.[0]
+  if (supportedBrowser) {
+    localStorage.setItem('lng', supportedBrowser)
+    return supportedBrowser
   }
 
   localStorage.setItem('lng', DefaultLocale)
