8be5b4b6fb5cf09ff5f7bb1f61ef7a8ac936cf8e
Ross Bulat <ross@jkrb.io>
2025-02-07 09:45:07 +0700

    Pie takes value

--- packages/ui-graphs/src/Pie/index.tsx
+++ packages/ui-graphs/src/Pie/index.tsx
@@ -3,51 +3,50 @@
 
 import { memo } from 'react'
 import styled, { keyframes } from 'styled-components'
-import type { PieProps } from '../types'
 import classes from './index.module.scss'
+import type { Props } from './types'
 
 // Memoizing component to prevent animation restarts
-export const Pie = memo(
-  ({
-    color1 = 'var(--accent-color-primary)',
-    color2 = 'var(--background-default)',
-  }: PieProps) => {
-    // Define the keyframes for the pie
-    const pie1Keyframes = keyframes`
+export const Pie = memo(({ value = 0 }: Props) => {
+  const inactive = 100 - value
+  const activeColor = 'var(--accent-color-primary)'
+  const inactiveColor = 'var(--background-default)'
+
+  // Define the keyframes for the pie
+  const pie1Keyframes = keyframes`
     100% {
-      stroke-dasharray: 10, 90, 0, 0;
+      stroke-dasharray: ${value}, ${inactive}, 0, 0;
       }
     `
-    const pie2Keyframes = keyframes`
+  const pie2Keyframes = keyframes`
     100% {
-      stroke-dasharray: 0, 10, 90, 0;
+      stroke-dasharray: 0, ${value}, ${inactive}, 0;
     }
   `
 
-    // Define pie animations per segment
-    const Wrapper = styled.div`
-      > svg {
-        > circle {
-          animation: ${pie1Keyframes} 0.75s 1 cubic-bezier(0, 0.85, 0, 0.85)
-            forwards;
-          &:nth-child(1) {
-            stroke: ${color1};
-          }
-          &:nth-child(2) {
-            stroke: ${color2};
-            animation-name: ${pie2Keyframes};
-          }
+  // Define pie animations per segment
+  const Wrapper = styled.div`
+    > svg {
+      > circle {
+        animation: ${pie1Keyframes} 0.75s 1 cubic-bezier(0, 0.85, 0, 0.85)
+          forwards;
+        &:nth-child(1) {
+          stroke: ${activeColor};
+        }
+        &:nth-child(2) {
+          stroke: ${inactiveColor};
+          animation-name: ${pie2Keyframes};
         }
       }
-    `
+    }
+  `
 
-    return (
-      <Wrapper className={classes.pie}>
-        <svg viewBox="0 0 63.6619772368 63.6619772368">
-          <circle cx="31.8309886184" cy="31.8309886184" r="15.9154943092" />
-          <circle cx="31.8309886184" cy="31.8309886184" r="15.9154943092" />
-        </svg>
-      </Wrapper>
-    )
-  }
-)
+  return (
+    <Wrapper className={classes.pie}>
+      <svg viewBox="0 0 63.6619772368 63.6619772368">
+        <circle cx="31.8309886184" cy="31.8309886184" r="15.9154943092" />
+        <circle cx="31.8309886184" cy="31.8309886184" r="15.9154943092" />
+      </svg>
+    </Wrapper>
+  )
+})
---
+++ packages/ui-graphs/src/Pie/types.ts
@@ -0,0 +1,6 @@
+// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+export interface Props {
+  value: number
+}
