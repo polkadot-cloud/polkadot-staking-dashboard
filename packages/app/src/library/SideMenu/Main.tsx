7867be24cb10e799872c2afb963e5efaeafa208e
Ross Bulat <ross@jkrb.io>
2025-02-18 12:18:10 +0700

    chore: side menu fixes

--- packages/app/src/library/SideMenu/Main.tsx
+++ packages/app/src/library/SideMenu/Main.tsx
@@ -1,41 +1,30 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { AnyJson } from '@w3ux/types'
 import type { PageCategory, PageItem, PagesConfigItems } from 'common-types'
 import { PageCategories, PagesConfig } from 'config/pages'
 import { useActiveAccounts } from 'contexts/ActiveAccounts'
 import { useBalances } from 'contexts/Balances'
 import { useBonded } from 'contexts/Bonded'
-import { useImportedAccounts } from 'contexts/Connect/ImportedAccounts'
-import { useNetwork } from 'contexts/Network'
 import { useActivePool } from 'contexts/Pools/ActivePool'
-import { useSetup } from 'contexts/Setup'
-import type { SetupContextInterface } from 'contexts/Setup/types'
 import { useStaking } from 'contexts/Staking'
 import { useUi } from 'contexts/UI'
 import type { UIContextInterface } from 'contexts/UI/types'
 import { useValidators } from 'contexts/Validators/ValidatorEntries'
 import { useSyncing } from 'hooks/useSyncing'
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom'
 import { Heading } from './Heading/Heading'
 import { Primary } from './Primary'
 
 export const Main = () => {
-  const { t, i18n } = useTranslation('base')
+  const { t } = useTranslation('base')
   const { syncing } = useSyncing()
   const { pathname } = useLocation()
   const { inPool } = useActivePool()
-  const { networkData } = useNetwork()
-  const {
-    getPoolSetupPercent,
-    getNominatorSetupPercent,
-  }: SetupContextInterface = useSetup()
   const { getNominations } = useBalances()
   const { getBondedAccount } = useBonded()
-  const { accounts } = useImportedAccounts()
   const { formatWithPrefs } = useValidators()
   const { activeAccount } = useActiveAccounts()
   const { sideMenuMinimised }: UIContextInterface = useUi()
@@ -49,64 +38,52 @@ export const Main = () => {
     (nominee) => nominee.prefs.commission === 100
   )
 
-  const [pageConfig, setPageConfig] = useState<AnyJson>({
-    categories: Object.assign(PageCategories),
-    pages: Object.assign(PagesConfig),
-  })
+  // Inject bullets into menu items
+  const pages: PageItem[] = [...PagesConfig]
 
-  // Configure side menu bullets for active account
-  useEffect(() => {
-    if (!accounts.length) {
-      return
-    }
-    // Inject actions into menu items
-    const pages: PageItem[] = Object.assign(pageConfig.pages)
-
-    let i = 0
-    for (const { uri } of pages) {
+  let i = 0
+  for (const { uri } of pages) {
+    const handleBullets = (): boolean => {
       if (uri === `${import.meta.env.BASE_URL}`) {
         const warning = !syncing && controllerDifferentToStash
         if (warning) {
           pages[i].bullet = 'warning'
+          return true
         }
       }
       if (uri === `${import.meta.env.BASE_URL}nominate`) {
-        const staking = !inNominatorSetup()
-        if (staking) {
+        if (!inNominatorSetup()) {
           pages[i].bullet = 'accent'
+          return true
         }
         if (
           (!syncing && controllerDifferentToStash) ||
           (!inNominatorSetup() && fullCommissionNominees.length > 0)
         ) {
           pages[i].bullet = 'warning'
+          return true
         }
       }
       if (uri === `${import.meta.env.BASE_URL}pools`) {
         if (inPool()) {
           pages[i].bullet = 'accent'
+          return true
         }
       }
-      i++
+      return false
     }
-    setPageConfig({
-      categories: pageConfig.categories,
-      pages,
-    })
-  }, [
-    networkData,
-    activeAccount,
-    accounts,
-    controllerDifferentToStash,
-    syncing,
-    inPool(),
-    inNominatorSetup(),
-    getNominatorSetupPercent(activeAccount),
-    getPoolSetupPercent(activeAccount),
-    i18n.resolvedLanguage,
-  ])
 
-  // remove pages that network does not support
+    handleBullets()
+    if (!handleBullets()) {
+      pages[i].bullet = undefined
+    }
+    i++
+  }
+  const pageConfig = {
+    categories: PageCategories,
+    pages,
+  }
+
   const pagesToDisplay: PagesConfigItems = Object.values(pageConfig.pages)
 
   return (
