0e406f22d683e02eb240994b8f10df6929e01797
Ross Bulat <ross@jkrb.io>
2025-02-18 11:56:26 +0700

    structural improvements

--- packages/ui-core/src/base/Page/Container/index.tsx
+++ packages/ui-core/src/base/Page/Container/index.tsx
@@ -1,19 +1,16 @@
-// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
-// SPDX-License-Identifier: GPL-3.0-only
-
 import type { ComponentBase } from '@w3ux/types'
-import { MaxPageWidth } from 'consts'
+import classNames from 'classnames'
 import classes from './index.module.scss'
 
 /**
  * @name Container
  * @summary Page container.
  */
-export const Container = ({ children, style }: ComponentBase) => (
-  <div
-    className={classes.container}
-    style={{ ...style, maxWidth: `${MaxPageWidth}px` }}
-  >
-    {children}
-  </div>
-)
+export const Container = ({ children, style }: ComponentBase) => {
+  const allClasses = classNames(classes.container, 'container-width')
+  return (
+    <div className={allClasses} style={{ ...style }}>
+      {children}
+    </div>
+  )
+}
