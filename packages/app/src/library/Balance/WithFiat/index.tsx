3a4d98415813c86a35cbf86ae3ef251f8a9f00a0
Ross Bulat <ross@jkrb.io>
2025-03-06 13:33:07 +0000

    update odometer, `WithFiat` on Overview page

--- packages/app/src/library/Balance/WithFiat/index.tsx
+++ packages/app/src/library/Balance/WithFiat/index.tsx
@@ -5,23 +5,36 @@ import { Odometer } from '@w3ux/react-odometer'
 import { minDecimalPlaces } from '@w3ux/utils'
 import BigNumber from 'bignumber.js'
 import { useNetwork } from 'contexts/Network'
+import { usePlugins } from 'contexts/Plugins'
 import type { ReactNode } from 'react'
-import { TokenFiat } from 'ui-core/base'
+import { CardLabel, TokenFiat } from 'ui-core/base'
 import { Value } from '../Value'
 
 export const WithFiat = ({
   Token,
   value,
   currency,
+  label,
 }: {
   Token: ReactNode
   value: number
   currency: string
+  label?: string
 }) => {
   const {
+    network,
     networkData: { units },
   } = useNetwork()
+  const { pluginEnabled } = usePlugins()
 
+  const showFiat = pluginEnabled('staking_api') && network !== 'westend'
+
+  // If the plugin is not enabled, or the network is Westend, we don't show the fiat value
+  if (!showFiat) {
+    return <Value tokenBalance={value} currency={currency} />
+  }
+
+  // Show token balance with fiat value
   return (
     <TokenFiat Token={Token}>
       <h1>
@@ -29,6 +42,8 @@ export const WithFiat = ({
           value={minDecimalPlaces(new BigNumber(value).toFormat(units), 2)}
           zeroDecimals={2}
         />
+
+        {label && <CardLabel>&nbsp;{label}</CardLabel>}
       </h1>
       <h3>
         <Value tokenBalance={value} currency={currency} />
