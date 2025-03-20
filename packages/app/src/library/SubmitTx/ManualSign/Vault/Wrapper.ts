f869d4e504634da474e946bcbff8d02673fa22d2
Ross Bulat <ross@jkrb.io>
2025-03-20 19:47:09 +0700

    mv Wrapper

--- packages/app/src/library/SubmitTx/ManualSign/Vault/Wrapper.ts
+++ packages/app/src/library/SubmitTx/ManualSign/Vault/Wrapper.ts
@@ -3,7 +3,7 @@
 
 import styled from 'styled-components'
 
-export const QRViewerWrapper = styled.div`
+export const Wrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
