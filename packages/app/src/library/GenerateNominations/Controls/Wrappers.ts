9f7d5e95e77e1a8426a3ddc458e9fce01218a59a
Ross Bulat <ross@jkrb.io>
2025-03-30 12:24:26 +0700

    chore: styling

--- packages/app/src/library/GenerateNominations/Controls/Wrappers.ts
+++ packages/app/src/library/GenerateNominations/Controls/Wrappers.ts
@@ -7,7 +7,6 @@ export const MenuWrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  padding: 0 1.5rem;
   background: var(--background-default);
   height: 3.25rem;
 `
