1e95683644331ae98d3ad477008d0c54a2248347
Ross Bulat <ross@jkrb.io>
2025-05-22 17:24:47 +0700

    make canvas scroll full width

--- packages/ui-core/src/canvas/Content/index.tsx
+++ packages/ui-core/src/canvas/Content/index.tsx
@@ -1,11 +1,22 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import classNames from 'classnames'
 import type { ComponentBase } from 'types'
 import classes from './index.module.scss'
 
-export const Content = ({ children, style }: ComponentBase) => (
-  <div className={classes.content} style={style}>
-    {children}
-  </div>
-)
+export const Content = ({
+  children,
+  style,
+  size,
+}: ComponentBase & { size?: string }) => {
+  const allClasses = classNames(classes.content, {
+    [classes.lg]: size === 'lg',
+    [classes.xl]: size === 'xl',
+  })
+  return (
+    <div className={allClasses} style={style}>
+      {children}
+    </div>
+  )
+}
