f18188eff04b07970f0b34ebdc1f0e388ce7b227
Ross Bulat <ross@jkrb.io>
2025-05-22 17:28:45 +0700

    canvas use `SimpleBar`

--- packages/ui-core/src/canvas/Scroll/index.tsx
+++ packages/ui-core/src/canvas/Scroll/index.tsx
@@ -2,11 +2,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { motion } from 'framer-motion'
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
 import type { ScrollProps } from '../types'
 import classes from './index.module.scss'
 
 export const Scroll = ({ children, ...rest }: ScrollProps) => (
   <motion.div className={classes.scroll} {...rest}>
-    {children}
+    <SimpleBar
+      autoHide={true}
+      style={{
+        height: '100%',
+      }}
+    >
+      {children}
+    </SimpleBar>
   </motion.div>
 )
