961328d67333e67aa700c4ccb3babb1f26c0cd2c
Ross Bulat <ross@jkrb.io>
2025-05-25 09:48:10 +0700

    chore: use dedot `startWithWorker`

--- packages/dedot-api/src/smoldot/providers.ts
+++ packages/dedot-api/src/smoldot/providers.ts
@@ -2,34 +2,16 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { SmoldotProvider } from 'dedot'
-import type { Client, SmoldotBytecode } from 'smoldot'
-import { startWithBytecode } from 'smoldot/no-auto-bytecode'
+import { startWithWorker } from 'dedot/smoldot/with-worker'
 import type { Network, SystemChain } from 'types'
-import type { WorkerOpts } from './types'
 
 // Instantiate smoldot from worker
 const initSmWorker = () => {
   const smWorker = new Worker(new URL('./worker.js', import.meta.url), {
     type: 'module',
   })
-  const client = doInitSmWorker(smWorker)
+  const client = startWithWorker(smWorker)
   return client
-}
-
-// Instantiate smoldot client
-//
-// Based on the example of smoldot from worker documentation at:
-// <https://github.com/smol-dot/smoldot/tree/main/wasm-node/javascript#usage-with-a-worker>
-export const doInitSmWorker = (
-  worker: Worker,
-  opts: WorkerOpts = {}
-): Client => {
-  const bytecode = new Promise<SmoldotBytecode>(
-    (resolve) => (worker.onmessage = ({ data }) => resolve(data))
-  )
-  const { port1, port2: portToWorker } = new MessageChannel()
-  worker.postMessage(port1, [port1])
-  return startWithBytecode({ bytecode, portToWorker, ...opts })
 }
 
 // Instantiate a new relay chain smoldot provider
