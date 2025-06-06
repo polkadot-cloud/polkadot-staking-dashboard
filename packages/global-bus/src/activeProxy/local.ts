a58a4ea0818e3096ee2f6508afb72d3f49ad0b03
Ross Bulat <ross@jkrb.io>
2025-06-06 12:26:12 +0700

    add `activeProxy` state to `global-bus`

---
+++ packages/global-bus/src/activeProxy/local.ts
@@ -0,0 +1,33 @@
+// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { localStorageOrDefault } from '@w3ux/utils'
+import { ActiveProxiesKey } from 'consts'
+import type { ActiveProxy, LocalActiveProxies, NetworkId } from 'types'
+
+export const setLocalActiveProxy = (
+  network: NetworkId,
+  proxy: ActiveProxy | null
+) => {
+  if (!proxy) {
+    removeLocalActiveProxy(network)
+  } else {
+    const proxies = { ...getLocalActiveProxies() }
+    proxies[network] = proxy
+    localStorage.setItem(ActiveProxiesKey, network)
+  }
+}
+
+export const removeLocalActiveProxy = (network: NetworkId) => {
+  const proxies = { ...getLocalActiveProxies() }
+  delete proxies[network]
+  localStorage.setItem(ActiveProxiesKey, JSON.stringify(proxies))
+}
+
+export const getLocalActiveProxies = () =>
+  localStorageOrDefault(ActiveProxiesKey, {}, true) as LocalActiveProxies
+
+export const getLocalActiveProxy = (network: NetworkId): ActiveProxy | null => {
+  const proxies = getLocalActiveProxies()
+  return proxies[network] || null
+}
