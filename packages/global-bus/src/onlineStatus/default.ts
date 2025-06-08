1040fe7fd6ea90e2fd586166aaea02caf4b4416a
Ross Bulat <ross@jkrb.io>
2025-06-08 17:46:45 +0700

    chore: online atatus to `global-bus`

--- packages/global-bus/src/onlineStatus/default.ts
+++ packages/global-bus/src/onlineStatus/default.ts
@@ -1,6 +1,6 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-export interface OnlineStatusEvent {
-  online: boolean
+export const defaultOnlineStatus = {
+  online: true,
 }
