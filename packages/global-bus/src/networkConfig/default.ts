9ec7c5c120512982381420429080fd2eb4d60a64
Ross Bulat <ross@jkrb.io>
2025-06-14 12:25:41 +0700

    fix missing license

--- packages/global-bus/src/networkConfig/default.ts
+++ packages/global-bus/src/networkConfig/default.ts
@@ -1,3 +1,6 @@
+// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import { DefaultNetwork } from 'consts/networks'
 import { getInitialProviderType } from './util'
 
