de4c840c38de25dc02fa0fcb270abbb0e9c69b02
Ross Bulat <ross@jkrb.io>
2025-06-14 11:03:31 +0700

    add `rpcEndpointHealth` query

---
+++ packages/plugin-staking-api/src/queries/rpcEndpointHealth.ts
@@ -0,0 +1,44 @@
+// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { gql, useQuery } from '@apollo/client'
+import { client } from '../Client'
+import type { RpcEndpointHealthResult } from '../types'
+
+const QUERY = gql`
+  query RpcEndpointHealth($network: String!) {
+    rpcEndpointHealth(network: $network) {
+      chains {
+        endpoints
+        chain
+      }
+    }
+  }
+`
+
+export const useRpcEndpointHealth = ({
+  network,
+}: {
+  network: string
+}): RpcEndpointHealthResult => {
+  const { loading, error, data, refetch } = useQuery(QUERY, {
+    variables: { network },
+  })
+  return { loading, error, data, refetch }
+}
+
+export const fetcRpcEndpointHealth = async (
+  network: string,
+  who: string,
+  eras: number
+) => {
+  try {
+    const result = await client.query({
+      query: QUERY,
+      variables: { network, who, eras },
+    })
+    return result.data.rpcEndpointHealth
+  } catch (error) {
+    return null
+  }
+}
