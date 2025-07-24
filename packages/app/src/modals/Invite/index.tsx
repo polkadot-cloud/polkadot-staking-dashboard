4b4b56d84d8ec125583acc26bee84b4a40f32ad2
Ross Bulat <ross@jkrb.io>
2025-07-24 10:36:26 +0700

    enable nominee address copy

--- packages/app/src/modals/Invite/index.tsx
+++ packages/app/src/modals/Invite/index.tsx
@@ -1,11 +1,15 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelopeOpenText,
+  faShareFromSquare,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useActiveAccounts } from 'contexts/ActiveAccounts'
 import { useBalances } from 'contexts/Balances'
 import { useNetwork } from 'contexts/Network'
+import { useValidators } from 'contexts/Validators/ValidatorEntries'
 import { ButtonCopy } from 'library/ButtonCopy'
 import { Title } from 'library/Modal/Title'
 import { useTranslation } from 'react-i18next'
@@ -14,17 +18,32 @@ import { Padding, Support } from 'ui-core/modal'
 export const Invite = () => {
   const { network } = useNetwork()
   const { t } = useTranslation()
-  const { getPoolMembership } = useBalances()
+  const { formatWithPrefs } = useValidators()
   const { activeAddress } = useActiveAccounts()
+  const { getPoolMembership, getNominations } = useBalances()
+
+  const nominated = formatWithPrefs(getNominations(activeAddress))
   const { membership } = getPoolMembership(activeAddress)
   const poolId = membership?.poolId || 0
 
+  const canCopy = nominated.length > 0 || membership !== undefined
+
   let inviteLink = undefined
+  let toCopy = ''
   let title = t('inviteStart', { ns: 'app' })
+  let subtitle = ''
+  let faIcon = faEnvelopeOpenText
 
   if (membership) {
     inviteLink = `https://staking.polkadot.cloud/#/overview?n=${network}&i=pool&id=${poolId}`
+    toCopy = inviteLink
+    subtitle = inviteLink
     title = t('copyPoolInviteLink', { ns: 'app' })
+  } else if (nominated.length > 0) {
+    faIcon = faShareFromSquare
+    toCopy = nominated.map((validator) => validator.address).join('\n')
+    title = t('copyAddresses', { ns: 'app' })
+    subtitle = t('copyValidatorAddresses', { ns: 'app' })
   }
 
   return (
@@ -32,11 +51,11 @@ export const Invite = () => {
       <Title />
       <Padding verticalOnly>
         <Support>
-          <FontAwesomeIcon icon={faEnvelopeOpenText} />
-          {inviteLink ? (
+          <FontAwesomeIcon icon={faIcon} />
+          {canCopy ? (
             <>
               <ButtonCopy
-                value={inviteLink}
+                value={toCopy}
                 size="1rem"
                 style={{ marginTop: '1.5rem' }}
                 children={
@@ -46,7 +65,7 @@ export const Invite = () => {
                   </h2>
                 }
               />
-              <p>{inviteLink}</p>
+              <p>{subtitle}</p>
             </>
           ) : (
             <>
