998d1ea4836b2c841677f0c4cc14b535d2705ea6
Ross Bulat <ross@jkrb.io>
2025-08-30 15:16:47 +0700

    rm help from simple nav

--- packages/app/src/library/SideMenu/NavSimple.tsx
+++ packages/app/src/library/SideMenu/NavSimple.tsx
@@ -6,17 +6,14 @@ import {
 	faChevronRight,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import DiscordSVG from 'assets/brands/discord.svg?react'
 import BookSVG from 'assets/icons/book.svg?react'
 import CloudSVG from 'assets/icons/cloud.svg?react'
-import EnvelopeSVG from 'assets/icons/envelope.svg?react'
 import LogoSVG from 'assets/icons/logo.svg?react'
 import { useHelp } from 'contexts/Help'
 import { useUi } from 'contexts/UI'
 import type { UIContextInterface } from 'contexts/UI/types'
 import { useTranslation } from 'react-i18next'
 import { Page } from 'ui-core/base'
-import { useOverlay } from 'ui-overlay'
 import { Main } from './Main'
 import { Secondary } from './Secondary'
 import { LogoWrapper, ToggleWrapper, Wrapper } from './Wrapper'
@@ -29,7 +26,6 @@ export const NavSimple = () => {
 		userSideMenuMinimised,
 		setUserSideMenuMinimised,
 	}: UIContextInterface = useUi()
-	const { openModal } = useOverlay().modal
 
 	return (
 		<>
@@ -73,24 +69,6 @@ export const NavSimple = () => {
 							icon={{
 								Svg: BookSVG,
 								size: sideMenuMinimised ? '0.95em' : '0.8em',
-							}}
-						/>
-						<Secondary
-							onClick={() => openModal({ key: 'DiscordSupport', size: 'sm' })}
-							name="Discord"
-							minimised={sideMenuMinimised}
-							icon={{
-								Svg: DiscordSVG,
-								size: sideMenuMinimised ? '1.2em' : '1em',
-							}}
-						/>
-						<Secondary
-							onClick={() => openModal({ key: 'MailSupport', size: 'sm' })}
-							name={t('email', { ns: 'app' })}
-							minimised={sideMenuMinimised}
-							icon={{
-								Svg: EnvelopeSVG,
-								size: sideMenuMinimised ? '1.05em' : '0.9em',
 							}}
 						/>
 					</div>
