c72d77afbb66ce3563c016e432e5380b8412a73c
Ross Bulat <ross@jkrb.io>
2025-10-21 16:52:21 +0700

    chore: add interface to `defaultNetworkConfig`

--- packages/global-bus/src/networkConfig/default.ts
+++ packages/global-bus/src/networkConfig/default.ts
@@ -1,9 +1,10 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import type { NetworkConfig } from 'types'
 import { getInitialNetwork, getInitialProviderType } from './util'
 
-export const defaultNetworkConfig = {
+export const defaultNetworkConfig: NetworkConfig = {
 	network: getInitialNetwork(),
 	rpcEndpoints: {},
 	providerType: getInitialProviderType(),
