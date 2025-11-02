69484086f45278d760480b488249036ea3d9fc17
Ross Bulat <ross@jkrb.io>
2025-11-02 13:31:20 +0700

    separate notifications from jsx

--- packages/app/src/library/Headers/Popovers/NotificationsPopover/index.tsx
+++ packages/app/src/library/Headers/Popovers/NotificationsPopover/index.tsx
@@ -11,7 +11,7 @@ import { type Dispatch, type SetStateAction, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { PopoverTab } from 'ui-buttons'
 import { Padding } from 'ui-core/popover'
-import { Pool } from './Pool'
+import { useOverlay } from 'ui-overlay'
 
 export const NotificationsPopover = ({
 	setOpen,
@@ -20,6 +20,7 @@ export const NotificationsPopover = ({
 }) => {
 	const { t } = useTranslation('app')
 	const { isBonding } = useStaking()
+	const { openCanvas } = useOverlay().canvas
 	const { getPoolMembership } = useBalances()
 	const { activeAddress } = useActiveAccounts()
 	const { inviteConfig, dismissInvite } = useInvites()
@@ -41,38 +42,75 @@ export const NotificationsPopover = ({
 		setOpen(false)
 	}, ['header-notifications'])
 
+	const notifications = []
+	if (inviteConfig) {
+		const actions = []
+		if (alreadyStaking) {
+			actions.push({
+				text: t('alreadyStaking', { ns: 'app' }),
+				onClick: () => {
+					// Do nothing
+				},
+				disabled: true,
+			})
+		} else {
+			actions.push({
+				text: t('viewInvite'),
+				onClick: () => {
+					setOpen(false)
+					openCanvas({
+						key: 'Pool',
+						options: {
+							providedPool: {
+								id: poolId,
+							},
+						},
+						size: 'xl',
+					})
+				},
+				disabled: false,
+			})
+		}
+
+		notifications.push({
+			title: `${t('invitePool')}!`,
+			actions: [
+				...actions,
+				{
+					text: t('dismiss'),
+					onClick: () => {
+						dismissInvite()
+						setOpen(false)
+					},
+					disabled: false,
+				},
+			],
+		})
+	}
+
 	return (
 		<div ref={popoverRef} style={{ paddingTop: '1.5rem' }}>
 			<Padding>
-				<h4>1 {t('notification', { count: 1 })}</h4>
-				<h3 style={{ margin: '0.75rem 0 1.5rem 0' }}>
-					{inviteConfig && inviteConfig.type === 'pool'
-						? t('invitePool')
-						: t('inviteNominate')}
-					!
-				</h3>
+				<h4>
+					{notifications.length}{' '}
+					{t('notification', { count: notifications.length })}
+				</h4>
+				{notifications.map((n) => (
+					<>
+						<h3 style={{ margin: '0.75rem 0 1.5rem 0' }}>{n.title}</h3>
+						<PopoverTab.Container position="bottom" yMargin>
+							{n.actions.map((action, index) => (
+								<PopoverTab.Button
+									key={`n_action_${index}`}
+									text={action.text}
+									onClick={() => action.onClick()}
+									disabled={action.disabled}
+								/>
+							))}
+						</PopoverTab.Container>
+					</>
+				))}
 			</Padding>
-			<PopoverTab.Container position="bottom" yMargin>
-				{alreadyStaking ? (
-					<PopoverTab.Button
-						text={t('alreadyStaking', { ns: 'app' })}
-						onClick={() => {
-							// Do nothing.
-						}}
-						disabled={true}
-					/>
-				) : poolId && !membership ? (
-					<Pool poolId={poolId} setOpen={setOpen} />
-				) : null}
-				<PopoverTab.Button
-					status="danger"
-					text={t('dismiss')}
-					onClick={() => {
-						dismissInvite()
-						setOpen(false)
-					}}
-				/>
-			</PopoverTab.Container>
 		</div>
 	)
 }
