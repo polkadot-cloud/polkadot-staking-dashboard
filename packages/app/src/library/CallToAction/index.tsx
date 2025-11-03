329dd3438a4b564a772d6e19b5342e87cb8d3ef3
Ross Bulat <ross@jkrb.io>
2025-11-03 18:23:42 +0700

    fix styling

--- packages/app/src/library/CallToAction/index.tsx
+++ packages/app/src/library/CallToAction/index.tsx
@@ -14,7 +14,7 @@ export const CallToActionWrapper = styled.div`
     flex: 1;
     display: flex;
     flex-direction: row;
-    flex-flow: row nowrap !important;
+    flex-flow: row wrap;
     width: 100%;
     margin-top: 0.38rem;   
 
@@ -40,15 +40,12 @@ export const CallToActionWrapper = styled.div`
       &:nth-child(1) {
         flex-grow: 1;
         @media (min-width: 651px) {
-          padding-right: 1rem;
-
           &.fixedWidth {
             border-right: 1px solid var(--border-primary-color);
             flex-grow: 0;
             flex-basis: 70%;
           }
         }
-
         @media (max-width: 650px) {
           &.fixedWidth {
             flex-basis: 100%;
@@ -58,7 +55,6 @@ export const CallToActionWrapper = styled.div`
 
       &:nth-child(2) {
         flex: 1;
-
         @media (min-width: 651px) {
           padding-left: 1rem;
         }
