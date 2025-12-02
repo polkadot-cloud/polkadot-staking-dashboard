3c79c11d331f075e78485c6cd00e95da496af306
Ross Bulat <ross@jkrb.io>
2025-12-02 11:23:14 +0700

    loader enhancements

--- packages/ui-core/src/base/Stat/Loading/index.tsx
+++ packages/ui-core/src/base/Stat/Loading/index.tsx
@@ -1,21 +1,30 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { Loader } from '../../Loader'
 import { Card } from '../Card'
 import { Content } from '../Content'
-import classes from './index.module.scss'
 
 export const Loading = () => {
 	return (
 		<Card>
 			<div>
 				<Content>
-					<div className={classes.shimmerTitle}>
-						<div className={classes.shimmerInner} />
-					</div>
-					<div className={classes.shimmerSubtitle}>
-						<div className={classes.shimmerInner} />
-					</div>
+					<Loader
+						style={{
+							height: '1.75rem',
+							width: '45%',
+							marginBottom: '0.5rem',
+							opacity: 0.4,
+						}}
+					/>
+					<Loader
+						style={{
+							height: '1.5rem',
+							width: '75%',
+							opacity: 0.4,
+						}}
+					/>
 				</Content>
 			</div>
 		</Card>
