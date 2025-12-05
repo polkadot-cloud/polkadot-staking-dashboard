208e397b9f57368ebac3db7a7ce7bc26455282df
Ross Bulat <ross@jkrb.io>
2025-12-05 16:32:04 +0700

    import fix

--- packages/app/src/library/Form/BalanceInput/types.ts
+++ packages/app/src/library/Form/BalanceInput/types.ts
@@ -1,6 +1,7 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import type BigNumber from 'bignumber.js'
 import type { ValueSetter } from '../types'
 
 export interface BalanceInputProps {
