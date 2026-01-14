2aa27799986603def8943fc84b14085874605219
Ross Bulat <ross@jkrb.io>
2026-01-14 16:15:08 +0700

    amend comment

--- packages/consts/src/nominate.ts
+++ packages/consts/src/nominate.ts
@@ -1,7 +1,7 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-// Recommend users to nominate with 150% of the minimum bond to earn rewards. This adds a
-// 50% buffer above the minimum and helps prevent situations where the bonded amount drops
-// below the minimum required to earn rewards in volatile network conditions.
+// The threshold above minimum to earn rewrads for a nominate recommendation. Acts as a buffer above
+// the minimum and helps prevent situations where the bonded amount drops below the minimum required
+// to earn rewards in volatile network conditions.
 export const NominateBuffer: bigint = 150n // 150%
