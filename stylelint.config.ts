caf2843c68e4e7073754e6bbc250dabd4db8d916
Ross Bulat <ross@jkrb.io>
2026-01-23 12:07:13 +0700

    fix license

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,3 +1,6 @@
+// Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { Config } from 'stylelint'
 
 export default {
