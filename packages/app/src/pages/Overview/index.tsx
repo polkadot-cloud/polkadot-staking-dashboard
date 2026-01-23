2a77be79bbdc0299c3f5e8fe54977e99b705c51c
Ross Bulat <ross@jkrb.io>
2026-01-23 17:41:54 +0700

    adjust height

--- packages/app/src/pages/Overview/index.tsx
+++ packages/app/src/pages/Overview/index.tsx
@@ -37,7 +37,7 @@ export const Overview = () => {
 	// Fiat values result in a slightly larger height for Balance & Payouts
 	const showFiat = pluginEnabled('staking_api') && network !== 'westend'
 
-	const STATUS_HEIGHT = 175
+	const STATUS_HEIGHT = 185
 	const PAYOUTS_HEIGHT = showFiat ? 385 : 380
 
 	return (
