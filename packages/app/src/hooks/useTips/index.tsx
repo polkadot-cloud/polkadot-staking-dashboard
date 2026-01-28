508a9c2cbe9559e9c99e1ea8e894027dbb2f5e1c
Ross Bulat <ross@jkrb.io>
2026-01-28 13:12:06 +0800

    remove icon

--- packages/app/src/hooks/useTips/index.tsx
+++ packages/app/src/hooks/useTips/index.tsx
@@ -1,7 +1,6 @@
 // Copyright 2025 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { TipsConfigAdvanced, TipsConfigSimple } from 'consts/tips'
 import { useActiveAccounts } from 'contexts/ActiveAccounts'
 import { useActivePool } from 'contexts/Pools/ActivePool'
@@ -136,7 +135,6 @@ export const useTips = () => {
 
 			return {
 				...filledVars,
-				faTipIcon: faExclamationTriangle,
 				onTipClick: () => {
 					openPromptWith(
 						<Tip
