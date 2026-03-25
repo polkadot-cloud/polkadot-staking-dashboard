d98c3c1e3f15712167ae91500725928aee167a6a
Ross Bulat <ross@jkrb.io>
2026-03-25 14:15:53 +0700

    feat(refactor): Optimise vite react plugin (#3375)

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,7 @@
 // Copyright 2026 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 import svgr from 'vite-plugin-svgr'
