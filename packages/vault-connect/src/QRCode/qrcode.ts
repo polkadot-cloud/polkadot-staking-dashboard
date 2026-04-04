b84c2451b60d953d633238b162525cc97bd91b79
Ross Bulat <ross@jkrb.io>
2026-04-04 15:05:47 +0700

    explan any

--- packages/vault-connect/src/QRCode/qrcode.ts
+++ packages/vault-connect/src/QRCode/qrcode.ts
@@ -5,6 +5,7 @@ import _qrcode from 'qrcode-generator'
 
 const qrcode: typeof _qrcode = _qrcode
 
+// biome-ignore lint/suspicious/noExplicitAny: <workaround for qrcode-generator types>
 ;(qrcode as any).stringToBytes = (data: Uint8Array): Uint8Array => data
 
 export { qrcode }
