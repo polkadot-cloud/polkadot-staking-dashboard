5c4938a37455f2c54c7a4e2f990b833c596353af
Ross Bulat <ross@jkrb.io>
2026-06-13 18:42:22 +0800

    `MaybeAddress` -> `string`

--- packages/app/src/library/NominatorList/types.ts
+++ packages/app/src/library/NominatorList/types.ts
@@ -1,10 +1,8 @@
 // Copyright 2026 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { MaybeAddress } from 'types'
-
 export interface NominatorListItemData {
-	address: MaybeAddress
+	address: string
 	label: string
 	stakedBalance: number
 	validatorApy: number
