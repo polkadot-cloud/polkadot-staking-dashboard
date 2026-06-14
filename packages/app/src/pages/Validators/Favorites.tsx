92440711b7328d25d3d67edb8ffe91f660a49211
Ross Bulat <ross@jkrb.io>
2026-06-14 14:13:51 +0800

    refactor contexts into hooks

--- packages/app/src/pages/Validators/Favorites.tsx
+++ packages/app/src/pages/Validators/Favorites.tsx
@@ -1,8 +1,8 @@
 // Copyright 2026 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { useFavoriteValidators } from 'contexts/Validators/FavoriteValidators'
 import { useApi } from 'hooks/useApi'
+import { useFavoriteValidators } from 'hooks/useFavoriteValidators'
 import { CardWrapper } from 'library/Card/Wrappers'
 import { ListStatusHeader } from 'library/List'
 import { ValidatorList } from 'library/ValidatorList'
--- packages/hooks/src/useStaking/types.ts
+++ packages/hooks/src/useStaking/types.ts
@@ -1,7 +1,7 @@
 // Copyright 2026 @polkadot-cloud/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-export interface StakingContextInterface {
+export interface StakingHookInterface {
 	isBonding: boolean
 	isNominating: boolean
 	isNominator: boolean
